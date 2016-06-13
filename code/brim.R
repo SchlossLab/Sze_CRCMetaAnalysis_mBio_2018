3fbb6a1e55de03ac6192b9d44fc280f4704f0404
marcsze <marcsze@med.umich.edu>
2016-06-12 20:21:53 -0400

    fixing path problems

--- code/brim.R
+++ code/brim.R
@@ -3,9 +3,9 @@
 	#  *Rows must be in the same order
 	#  *Metadata must contain sample id, white, and disease
 
-shared <- read.table("data/brim/test/combined.trim.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared", header=T)
+shared <- read.table("data/raw/brim/test/combined.trim.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared", header=T)
 
-metadata <- read.csv("data/brim/brim.metadata.csv", stringAsFactors=F)
+metadata <- read.csv("data/raw/brim/brim.metadata.csv", stringsAsFactors=F)
 
 stopifnot(shared$Group == metadata$sample)
 
@@ -15,7 +15,7 @@ disease <- metadata$disease
 
 metadata <- cbind(sample=sample, white=white, disease=disease)
 
-write.table(shared, file="data/brim/test/brim.shared", quote=F, sep='\t', row.names=F)
+write.table(shared, file="data/raw/brim/test/brim.shared", quote=F, sep='\t', row.names=F)
 
-write.table(metadata, file="data/brim/test/brim.metadata", quote=F, sep='\t', row.names=F)
+write.table(metadata, file="data/raw/brim/test/brim.metadata", quote=F, sep='\t', row.names=F)
  
