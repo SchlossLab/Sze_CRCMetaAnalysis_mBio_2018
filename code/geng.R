85ec86e27bd29fc90f04291c13d54df85e61d896
marcsze <marcsze@med.umich.edu>
2016-06-14 11:20:18 -0400

    fix typo

--- code/geng.R
+++ code/geng.R
@@ -3,9 +3,9 @@
 	#  *Rows must be in the same order
 	#  *Metadata must contain sample id, white, and disease
 
-shared <- read.table("data/process/tissue/geng/combined.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared", header=T)
+shared <- read.table("data/process/geng/combined.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared", header=T)
 
-metadata <- read.csv("data/process/tissue/geng/GengData.csv", stringsAsFactors=F)
+metadata <- read.csv("data/process/geng/GengData.csv", stringsAsFactors=F)
 
 metadata$disease[grep("H", metadata$Sample_Name_s)] <- "control"
 metadata$disease[grep("C", metadata$Sample_Name_s)] <- "cancer"
@@ -18,7 +18,7 @@ disease <- metadata$disease
 
 metadata <- cbind(sample=sample, white=white, disease=disease)
 
-write.table(shared, file="data/process/tissue/geng/geng.shared", quote=F, sep='\t', row.names=F)
+write.table(shared, file="data/process/geng/geng.shared", quote=F, sep='\t', row.names=F)
 
-write.table(metadata, file="data/process/tissue/geng/geng.metadata", quote=F, sep='\t', row.names=F)
+write.table(metadata, file="data/process/geng/geng.metadata", quote=F, sep='\t', row.names=F)
  
