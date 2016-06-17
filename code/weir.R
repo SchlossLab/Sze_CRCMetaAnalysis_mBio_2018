a8ce6a5fe8c748c4595ccdb98c8a9aa19501ff9b
marcsze <marcsze@med.umich.edu>
2016-06-17 08:37:32 -0400

    R metadata code

---
+++ code/weir.R
@@ -0,0 +1,23 @@
+#Output shared file that has the same rows as the metadata file. 
+#Requirements:
+	#  *Rows must be in the same order
+	#  *Metadata must contain sample id, white, and disease
+
+shared <- read.table("data/process/weir/combined.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared", header=T)
+
+metadata <- read.csv("data/process/weir/weir.raw.metadata.csv", stringsAsFactors=F)
+
+stopifnot(shared$Group == metadata$SampleID)
+
+sample <- metadata$SampleID
+white <- metadata$white
+disease <- metadata$disease
+sex <- metadata$sex
+age <- metadata$age
+
+metadata <- cbind(sample=sample, white=white, disease=disease, sex=sex, age=age)
+
+write.table(shared, file="data/process/weir/weir.shared", quote=F, sep='\t', row.names=F)
+
+write.table(metadata, file="data/process/weir/weir.metadata", quote=F, sep='\t', row.names=F)
+ 
