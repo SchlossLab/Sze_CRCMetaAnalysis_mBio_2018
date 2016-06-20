d81397e44f3808387d9ab9ec237b053b70290134
marcsze <marcsze@med.umich.edu>
2016-06-20 10:01:11 -0400

    add metadata R code

---
+++ code/baxter.R
@@ -0,0 +1,32 @@
+#Output shared file that has the same rows as the metadata file. 
+#Requirements:
+	#  *Rows must be in the same order
+	#  *Metadata must contain sample id, white, and disease
+
+shared <- read.table("data/process/baxter/glne007.final.an.unique_list.shared", header=T)
+
+metadata <- read.delim("data/process/baxter/metadata.tsv", header=T, stringsAsFactors=F)
+
+#Get only relevant samples
+rownames(shared) <- shared$Group
+rownames(metadata) <- metadata$sample
+shared <- shared[rownames(metadata), ]
+
+metadata$dx[which(metadata$dx == "normal")] <- "control" 
+metadata$dx[which(metadata$dx == "adenoma")] <- "polyp"
+
+#Check that everything is okay
+stopifnot(shared$Group == metadata$sample)
+
+sample <- metadata$sample
+white <- metadata$White
+disease <- metadata$dx
+age <- metadata$Age
+sex <- metadata$Gender
+
+metadata <- cbind(sample=sample, white=white, disease=disease, age=age, sex=sex)
+
+write.table(shared, file="data/process/baxter/baxter.shared", quote=F, sep='\t', row.names=F)
+
+write.table(metadata, file="data/process/baxter/baxter.metadata", quote=F, sep='\t', row.names=F)
+ 
