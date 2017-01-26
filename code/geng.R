6641b0b1f41806d69e60219b9ebdccb0b46232a1
marcsze <marcsze@med.umich.edu>
2017-01-26 14:40:48 -0500

    change imported file title

--- code/geng.R
+++ code/geng.R
@@ -3,7 +3,7 @@
 	#  *Rows must be in the same order
 	#  *Metadata must contain sample id, white, and disease
 
-shared <- read.table("data/process/geng/combined.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared", header=T)
+shared <- read.table("data/process/geng/combined.unique.good.filter.unique.precluster.pick.pick.opti_mcc.unique_list.shared", header=T)
 
 metadata <- read.csv("data/process/geng/GengData.csv", stringsAsFactors=F)
 
