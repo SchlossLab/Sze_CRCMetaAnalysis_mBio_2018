e1672bf69d97bce6840a5267f8331143a46872cf
Marc Sze <marcsze@flux-login1.arc-ts.umich.edu>
2017-05-23 07:27:55 -0400

    update to opticlust method

--- code/brim.R
+++ code/brim.R
@@ -3,9 +3,11 @@
 	#  *Rows must be in the same order
 	#  *Metadata must contain sample id, white, and disease
 
-shared <- read.table("data/process/brim/combined.trim.unique.good.filter.unique.precluster.pick.pick.an.unique_list.shared", header=T)
+shared <- read.table("data/process/brim/combined.trim.unique.good.filter.unique.precluster.pick.pick.opti_mcc.unique_list.shared", 
+	stringsAsFactors=F, header=T)
 
-metadata <- read.csv("data/process/brim/brim.metadata.csv", stringsAsFactors=F)
+metadata <- read.csv("data/process/brim/brim.metadata.csv", 
+	stringsAsFactors=F, header=T)
 
 stopifnot(shared$Group == metadata$sample)
 
