d24603cec57c6036a1ffaa9c0ab4c003cc832f3c
marcsze <marcasze@gmail.com>
2017-07-03 11:28:37 -0400

    put up basic framework for data combo and transformation

--- code/get_alpha.R
+++ code/get_alpha.R
@@ -7,14 +7,27 @@ source('code/functions.R')
 
 loadLibs(c("dplyr", "tidyr", "car"))
 
+# Need to loop through and do this for every data set
 
+
+# Load in alpha metrics to be used
 test_data <- read.delim("data/process/ahn/ahn.groups.ave-std.summary", 
                       header = T, stringsAsFactors = F) %>% 
-  filter(method == "ave")
+  filter(method == "ave") %>% 
+  select(group, sobs, shannon, shannoneven)
 
-
+# Load in metadata and match
+meta_data <- read.delim("data/process/ahn/ahn.metadata", 
+                        header = T, stringsAsFactors = F) %>% 
+  slice(match(test_data$group, sample))
 
 # Can use scale to z-score normalize
+test_data <- test_data %>% inner_join(meta_data, by = c("group" = "sample")) %>% 
+  mutate_at(vars(sobs:shannoneven), function(x) as.vector(scale(x)))
+  
+
+
+
 
 
 # Can use powerTransform instead to try and correct out skew
