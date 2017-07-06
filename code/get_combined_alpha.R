b9ce5325f25e5b01a9804795165faebf75b4e49d
marcsze <marcasze@gmail.com>
2017-07-06 15:30:31 -0400

    updated code

--- code/get_combined_alpha.R
+++ code/get_combined_alpha.R
@@ -22,74 +22,66 @@ stool_sets <- c("wang", "brim", "weir", "ahn", "zeller", "baxter")
 # Flemer, Chen
 both_sets <- c("flemer", "chen")
 
-# Create function to wrangle the data
-get_combined_table <- function(datasets, sample_source, split_on = NULL){
-  
-  # set up list to store the stool alpha, meta, and combined z-score with meta data
-  all_data <- as.list(datasets)
-  names(all_data) <- datasets
-  all_metdata <- all_data
-  all_z_alpha <- all_data
-  
-  # Run the for loop to process the data
-  for(i in 1:length(datasets)){
-    
-    # Load in alpha metrics to be used for stool
-    all_data[[i]] <- read.delim(paste("data/process/", datasets[i], "/", datasets[i], 
-                                      ".groups.ave-std.summary", sep = ""), 
-                                header = T, stringsAsFactors = F) %>% 
-      filter(method == "ave") %>% 
-      mutate(group = as.character(group)) %>% 
-      select(group, sobs, shannon, shannoneven)
-    
-    # Put in argument that if data set needs pre-splitting go here first
-    if(!is.null(split_on)){
-      
-      # Load in metadata and match
-      all_metdata[[i]] <- read.delim(
-        paste("data/process/", 
-              datasets[i], "/", datasets[i], ".metadata", sep = ""), 
-        header = T, stringsAsFactors = F) %>% 
-        filter(sample_type == split_on) %>% 
-        mutate(sample = as.character(sample)) %>% 
-        slice(match(all_data[[i]]$group, sample))
-      
-    } else{
-      
-      # Load in metadata and match (will only use this if sample type is all one type)
-      all_metdata[[i]] <- read.delim(
-        paste("data/process/", 
-              datasets[i], "/", datasets[i], ".metadata", sep = ""), 
-        header = T, stringsAsFactors = F) %>% 
-        mutate(sample = as.character(sample)) %>% 
-        slice(match(all_data[[i]]$group, sample))
-      
-    }
-    
 
-    # Can use scale to z-score normalize
-    all_z_alpha[[i]] <- all_data[[i]] %>% 
-      inner_join(all_metdata[[i]], by = c("group" = "sample")) %>% 
-      mutate_at(vars(sobs:shannoneven), function(x) as.vector(scale(x))) %>% 
-      mutate(set = datasets[i], sample_type = sample_source)
+get_Z_data <- function(i, sample_source){
+  # i represents a character vector with data sets that should be worked through
+  # sample_source represents the type of sample e.g. stool or tissue
+    # splitting will be done based on this call 
+  
+  # Load in alpha metrics to be used for stool
+  all_data <- read.delim(paste("data/process/", i, "/", i, 
+                                    ".groups.ave-std.summary", sep = ""), 
+                              header = T, stringsAsFactors = F) %>% 
+    filter(method == "ave") %>% 
+    mutate(group = as.character(group)) %>% 
+    select(group, sobs, shannon, shannoneven)
+  
+  
+  # Load in metadata and match
+  all_metdata <- read.delim(
+    paste("data/process/", i, "/", i, ".metadata", sep = ""), 
+    header = T, stringsAsFactors = F) 
+  
+  # Create a new column called sample_type if it is not already present
+  if(!("sample_type" %in% colnames(all_metdata))){
     
+    all_metdata <- all_metdata %>% mutate(sample_type = sample_source)
   }
   
-  # Combine data together
-  data_z_combined <- c()
+  # filter based on sample type and match metadata with all alpha data
+  all_metdata <- all_metdata %>%   
+    mutate(sample = as.character(sample)) %>% 
+    filter(sample_type == sample_source) %>% 
+    slice(match(all_data$group, sample))
+  
+  # Can use scale to z-score normalize
+  all_z_alpha <- all_data %>% 
+    inner_join(all_metdata, by = c("group" = "sample")) %>% 
+    mutate_at(vars(sobs, shannon, shannoneven), function(x) as.vector(scale(x))) %>% 
+    mutate(study = i)
+  
+  return(all_z_alpha)
+}
+
+
+# Create function to wrangle the data
+get_combined_table <- function(datasets, sample_source){
+  # i represents a character vector with data sets that should be worked through
+  # sample_source represents the type of sample e.g. stool or tissue
+    # splitting will be done based on this call 
+  
+  # Get Z-score transformed alpha with relevant metadata
+  all_z_alpha <- mapply(get_Z_data, datasets, sample_source)
+  
   # combine all the different data sets together
-  for(i in 1:length(datasets)){
-    
-    data_z_combined <- data_z_combined %>% 
-      bind_rows(mutate(all_z_alpha[[i]]))
-  }
-  
+  data_z_combined <- bind_rows(lapply(all_z_alpha, function(x) as.data.frame(x)))
+
   
   # convert sex column so that all entries are uniform
   data_z_combined <- data_z_combined %>% 
-    mutate(sex = gsub("female", "f", sex), 
-           sex = gsub("male", "m", sex), 
-           sex = gsub("<not provided>", NA, sex))
+    mutate(sex = gsub("f.*", "f", sex, ignore.case = T), 
+           sex = gsub("m.*", "m", sex, ignore.case = T), 
+           sex = gsub("^(?!m|f).*$", NA, sex, perl = T, ignore.case = T))
   
   # Write out completed data table
   return(data_z_combined)
@@ -124,18 +116,18 @@ generate_histogram_alpha <- function(data_set, sample_type){
 
 
 # Create combined stool data table
-stool_data <- get_combined_table(stool_sets, "stool", split_on = NULL) %>% 
-  bind_rows(get_combined_table(both_sets, "stool", split_on = "stool")) %>% 
-  select(group, sobs, shannon, shannoneven, disease, white, set, sample_type, sex, 
-         age, bmi) %>% 
+stool_data <- get_combined_table(stool_sets, "stool") %>% 
+  bind_rows(get_combined_table(both_sets, "stool")) %>% 
+  select(group, sobs, shannon, shannoneven, disease, white, sample_type, sex, 
+         age, bmi, study) %>% 
   filter(!is.na(disease))
 
 
 # Create combined tissue data table
-tissue_data <- get_combined_table(tissue_sets, "tissue", split_on = NULL) %>% 
-  bind_rows(get_combined_table(both_sets, "tissue", split_on = "tissue")) %>% 
-  select(group, sobs, shannon, shannoneven, white, disease, matched, set, sample_type, 
-         age, sex, site, stage, size_mm, bmi)
+tissue_data <- get_combined_table(tissue_sets, "tissue") %>% 
+  bind_rows(get_combined_table(both_sets, "tissue")) %>% 
+  select(group, sobs, shannon, shannoneven, white, disease, matched, sample_type, 
+         age, sex, site, stage, size_mm, bmi, study)
 
 
 # Create graphs
