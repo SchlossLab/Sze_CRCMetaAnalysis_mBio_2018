beb620118a52466fdff222f648be09bd9368024d
marcsze <marcasze@gmail.com>
2017-08-17 08:02:21 -0400

    add comments to code

--- code/get_beta_stool.R
+++ code/get_beta_stool.R
@@ -24,9 +24,13 @@ both_sets <- c("flemer")
 
 # Create function to read in distance data
 get_distance <- function(i, path_to_file, file_ending){
+  # i is the respective study
+  # path_to_file is the path to the dir where file is stored
+  # file_ending customizes the type of file that needs to be pulled
   
+  # reads in the distance file based on variables provided
   tempDist <- read.dist(paste(path_to_file, i, "/", i, ".", file_ending, sep = ""))
-  
+  # returns the read in distance file
   return(tempDist)
 }
 
@@ -37,7 +41,7 @@ get_metadata_data <- function(i, sampleType){
   # i represents the data set
   # sampleType represents whether it is stool or tissue
   
-  # Command that actually does the reading in
+  # Command that actually does the reading in and removes uneeded alpha metrics
   data_list <- read.csv(paste("data/process/tables/", i, "_", sampleType, "_",  
                               "transformed_data_alpha_raw_values.csv", sep = ""), 
                         header = T, stringsAsFactors = F) %>% 
@@ -50,25 +54,32 @@ get_metadata_data <- function(i, sampleType){
 
 # Function to make meta data the same order as that seen in the distance matrix
 reorder_meta <- function(i, distanceList = distance_data, metaList = metadata){
+  # i is the study
+  # distanceList is defaulted to distance_data to make running mapply easier
+  # metaList is defaulted to metadata to make running mapply easier
   
+  # pulls the specific study distance matrix group order
   dist_names <- as.character(rownames(distanceList[[i]]))
-  
+  # aligns meta data by group order in distance matrix and creates a new is.cancer variable
   new_meta <- metaList[[i]] %>% slice(match(dist_names, as.character(group))) %>% 
     mutate(is_cancer =  factor(ifelse(disease == "cancer", 
                                       invisible("Y"), invisible("N")), levels = c("Y", "N")))
-
+  # returns the modified metadata file
   return(new_meta)
 }
 
 
-
-# Function to align distance matrices
+# Function to align distance matrices with the metadata file
 reorder_dist <- function(i, distanceList = distance_data, metaList = reordered_meta){
+  # i is the study
+  # distanceList is defaulted to distance_data to make running mapply easier
+  # metaList is defaulted to reordered_meta so that the aligned metalist is used
   
+  # grabs the samples in the meta file
   samples_to_keep <- as.character(metaList[[i]]$group)
-  
+  # subsets the distance matrix by the grabbed metafile samples
   new_distance_tbl <- distanceList[[i]][samples_to_keep, samples_to_keep]
-  
+  # returns the new updated distance matrix
   return(new_distance_tbl)
 }
 
@@ -76,21 +87,27 @@ reorder_dist <- function(i, distanceList = distance_data, metaList = reordered_m
 # Generate the PERMANOVA comparisons
 make_adonis_test <- function(i, distanceList = reordered_dist, 
                              metaList = reordered_meta){
+  # i is the study
+  # distanceList is defaulted to reordered_dist so the subsetted dist file is used
+  # metaList is defaulted to reordered_meta so the aligned metalist file is used
   
+  # set the seed number so results don't keep changing
   set.seed(1234567)
+  # run the permanova test using vegan for the selected data set
   temptest <- adonis(as.dist(distanceList[[i]]) ~ metaList[[i]]$is_cancer, 
                      permutations = 9999)
-  
+  # pull only results of interest to be saved
   result_vector <- c(fstat = temptest$aov.tab$F.Model[1], r2 = temptest$aov.tab$R2[1], 
                      pvalue = temptest$aov.tab$`Pr(>F)`[1])
   
-  
+  # return the results of interest
   return(result_vector)
-  
 }
 
 
-
+##############################################################################################
+############### Run the actual programs to get the data ######################################
+##############################################################################################
 
 
 # Read in distance data
