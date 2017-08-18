eddd6c46cd31932003737c1ff39342c723b9d8ad
marcsze <marcasze@gmail.com>
2017-08-18 08:07:12 -0400

    modify code to have raw values on output table

--- code/get_matched_tissue.R
+++ code/get_matched_tissue.R
@@ -49,6 +49,77 @@ get_pwr_transformed_data <- function(i){
 }
 
 
+get_data <- function(i, sample_source){
+  # i represents a character vector with data sets that should be worked through
+  # sample_source represents the type of sample e.g. stool or tissue
+  # splitting will be done based on this call 
+  
+  # Load in alpha metrics to be used for stool
+  all_data <- read.delim(paste("data/process/", i, "/", i, 
+                               ".groups.ave-std.summary", sep = ""), 
+                         header = T, stringsAsFactors = F) %>% 
+    filter(method == "ave") %>% 
+    mutate(group = as.character(group)) %>% 
+    select(group, sobs, shannon, shannoneven)
+  
+  
+  # Load in metadata and match
+  all_metdata <- read.delim(
+    paste("data/process/", i, "/", i, ".metadata", sep = ""), 
+    header = T, stringsAsFactors = F) 
+  
+  # Create a new column called sample_type if it is not already present
+  if(!("sample_type" %in% colnames(all_metdata))){
+    
+    all_metdata <- all_metdata %>% mutate(sample_type = sample_source)
+  }
+  # Create a new column called sex if it is not already present
+  if(!("sex" %in% colnames(all_metdata))){
+    
+    all_metdata <- all_metdata %>% mutate(sex = NA)
+  }
+  # Create a new column called age if it is not already present
+  if(!("age" %in% colnames(all_metdata))){
+    
+    all_metdata <- all_metdata %>% mutate(age = NA)
+  }
+  # Create a new column called bmi if it is not already present
+  if(!("bmi" %in% colnames(all_metdata))){
+    
+    all_metdata <- all_metdata %>% mutate(bmi = NA)
+  }
+  # Create a new column called white if it is not already present
+  if(!("white" %in% colnames(all_metdata))){
+    
+    all_metdata <- all_metdata %>% mutate(white = NA)
+  }
+  
+  # filter based on sample type and match metadata with all alpha data
+  combined_data <- all_metdata %>%   
+    mutate(sample = as.character(sample)) %>% 
+    filter(sample_type == sample_source) %>% 
+    slice(match(all_data$group, sample)) %>% 
+    mutate(study = i) %>% 
+    inner_join(all_data, by = c("sample" = "group"))
+  
+  # convert sex column so that all entries are uniform
+  combined_df <- combined_data %>% 
+    mutate(sex = gsub("f.*", "f", sex, ignore.case = T), 
+           sex = gsub("m.*", "m", sex, ignore.case = T), 
+           sex = gsub("^(?!m|f).*$", NA, sex, perl = T, ignore.case = T))
+  
+  # Select specific columns and rows for the final data table
+  combined_df <- combined_df %>% 
+    select(sample, sobs, shannon, shannoneven, disease, 
+           white, sample_type, sex, age, bmi, study) %>% 
+    filter(!is.na(disease)) %>% 
+    rename(group = sample)
+  
+  
+  return(combined_df)
+}
+
+
 # Function to read in complete tissue metadata
 get_orig_metadata <- function(i){
   
@@ -185,6 +256,19 @@ tissue_matched_data <- bind_rows(mapply(get_matched_set_data, matched_sets, "mat
 tissue_unmatched_data <- bind_rows(mapply(get_matched_set_data, matched_sets, "unmatched"), 
                   zscore_pwr_transform_data[c("chen", "flemer", "sana")]) %>% 
   mutate(disease = ifelse(is.na(disease), invisible(disease.x), invisible(disease)))
+
+
+# read in normal alpha data
+raw_alpha <- mapply(get_data, c(tissue_sets, both_sets), "tissue", SIMPLIFY = F) %>% 
+  bind_rows() %>% select(group, sobs, shannon, shannoneven) %>% 
+  rename(r_sobs = sobs, r_shannon = shannon, r_shannoneven = shannoneven)
+  
+# Join raw values with now transformed values
+tissue_matched_data <- tissue_matched_data %>% inner_join(raw_alpha, by = "group")
+tissue_unmatched_data <- tissue_unmatched_data %>% inner_join(raw_alpha, by = "group")
+
+
+
 
 
 # Write out the data tables for analysis
