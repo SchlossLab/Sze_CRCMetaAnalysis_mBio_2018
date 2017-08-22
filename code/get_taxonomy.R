d58859c0954ac98ef957600e4cf4717cce14a160
marcsze <marcasze@gmail.com>
2017-08-22 09:20:26 -0400

    first commit

---
+++ code/get_taxonomy.R
@@ -0,0 +1,23 @@
+### Generate genera files
+### Marc Sze
+
+
+# Load in needed functions and libraries
+source('code/functions.R')
+
+# Load needed libraries
+loadLibs(c("dplyr", "tidyr", "car", "ggplot2", "rcompanion"))
+
+# Tissue Only sets
+# Lu, Dejea, Sana, Burns, Geng
+tissue_sets <- c("lu", "dejea", "sana", "burns", "geng")
+
+# Stool Only sets
+# Hale, Wang, Brim, Weir, Ahn, Zeller, Baxter
+stool_sets <- c("wang", "brim", "weir", "ahn", "zeller", "baxter")
+
+# Both Tissue and Stool
+# flemer sampletype = biopsy or stool
+# chen sample_type = tissue or stool
+# Flemer, Chen
+both_sets <- c("flemer", "chen")
