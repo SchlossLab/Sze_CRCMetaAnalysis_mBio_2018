6923b5389030afdc2b80c6bffe55e342c1e51b6a
marcsze <marcasze@gmail.com>
2017-08-28 13:09:51 -0400

    first shared - meta alignment add

---
+++ code/hale.R
@@ -0,0 +1,31 @@
+### create metadata file for Hale, et al study 2016
+### Marc Sze
+
+### Some metadata is pulled from the study
+
+
+# Load needed libraries
+library(dplyr)
+library(tidyr)
+
+# Load needed data
+metadata <- read.csv("data/process/hale/hale_metadata.csv", 
+                     header = T, stringsAsFactors = F)
+
+# This is too big to load on laptop
+shared <- read.delim("data/process/hale/hale.trim.contigs.unique.good.filter.unique.precluster.pick.pick.opti_mcc.unique_list.shared", 
+                     header = T, stringsAsFactors = F)
+
+# Match shared with tempData
+select_meta <- metadata %>% slice(match(shared$Group, sampleID))
+
+
+# Check order is okay
+stopifnot(shared$Group == select_meta$samples)
+
+
+# Write out to table
+write.table(shared, file="data/process/hale/hale.shared", quote=F, sep='\t', row.names=F)
+
+write.table(select_meta, file="data/process/hale/hale.metadata", quote=F, sep='\t', row.names=F)
+
