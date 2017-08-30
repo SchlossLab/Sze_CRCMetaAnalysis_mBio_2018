0be07319dcb050d4a043d25748b7aa59dc91b901
marcsze <marcasze@gmail.com>
2017-08-30 08:58:01 -0400

    complete comments for get_data

--- code/get_genera_dmm.R
+++ code/get_genera_dmm.R
@@ -126,37 +126,40 @@ get_fisher_pvalue <- function(metaData){
 }
 
 
-# Control function to get all the data
+# Control function to get all the data, basically runs the above functions in a
+# contained location withouth having to repeat them
 get_data <- function(i){
+  # i is the study of interest
   
+  # gets original sample names
   sample_names <- (get_file(i, "data/process/", "_genera_shared.csv") %>% 
                      mutate(sample_names = rownames(.)) %>% select(sample_names))[, "sample_names"]
-  
+  # grabs subsampled data and assigns rownames from sample names to table
   sub_genera_data <- get_file(i, "data/process/", "_subsample_genera.csv", rows_present = F, 
                               vec_of_rownames = sample_names) %>% 
     as.data.frame() %>% mutate(sample_ID = rownames(.)) %>% 
     select(sample_ID, everything())
-  
+  # grabs the meta data and transforms polyp to control (polyp/control vs cancer) 
   study_meta <- get_file(i, "data/process/", ".metadata", rows_present = F,  
                          "stool", metadata = T) %>% 
     mutate(disease = ifelse(disease == "polyp", invisible("control"), invisible(disease)))
-  
+  # conditional that checks for whether length of rows of meta data is smaller
   if(length(rownames(study_meta)) < length(rownames(sub_genera_data))){
-    
+    # grab only the samples in the meta data file for down stream analysis
     sub_genera_data <- sub_genera_data %>% slice(match(study_meta$sampleID, sample_ID))
     
   } else{
-    
+    # grab only files in the data file for analysis
     study_meta <- study_meta %>% slice(match(sub_genera_data$sample_ID, sampleID))
   }
-  
+  # re assigns the rown names while removing the extra column used for sorting
   sample_names <- sub_genera_data$sample_ID
   sub_genera_data <- as.matrix(sub_genera_data %>% select(-sample_ID))
   rownames(sub_genera_data) <- sample_names
-  
+  # creates a list file with both data sets
   dataList <- list(sub_genera_data = sub_genera_data, 
                    study_meta = study_meta)
-  
+  # returns the combined list file
   return(dataList)
   
 }
