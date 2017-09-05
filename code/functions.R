7f374eedb7ca25025b1122a9a23fb6ddac008d4b
marcsze <marcasze@gmail.com>
2017-09-05 13:26:51 -0400

    add common function for data uploading

--- code/functions.R
+++ code/functions.R
@@ -130,6 +130,55 @@ get_tax_level_shared <- function(i, shared_List, tax_List, tax_level){
 
 
 
+# Function to read in needed genera file
+get_file <- function(i, path_to_file, ending, rows_present=T, name_of_rows=1, 
+                     vec_of_rownames = NULL, sample_source, metadata = F){
+  # i represents the study name
+  # path_to_file is the is the directory where the main studies are stored
+  # ending is the extension after i of the name of the file
+  # rows_present signifies whether rownames are part of the inputed file
+  # name_of_rows represents what column the row names are in
+  # sample_source signifies whether it is stool or tissue
+  # metadata signifies whether the inputed file is metadata or not
+  
+  # First conditional to make sure only data goes through this part
+  if(metadata == F){
+    # second conditional to check if rownames are present or not
+    if(rows_present == T){
+      # reads in data (assumes csv file) with row names from data table
+      temp_shared <- read.csv(paste(path_to_file, i, "/", i, ending, sep = ""), 
+                              header = T, stringsAsFactors = F, row.names = name_of_rows)
+    } else{
+      # reads in data (assumes csv file) with row names from an inputed vector 
+      temp_shared <- read.csv(paste(path_to_file, i, "/", i, ending, sep = ""), 
+                              header = T, stringsAsFactors = F)
+      rownames(temp_shared) <- vec_of_rownames
+    }
+    
+  } else{
+    # reads in data table (assumes tab delimited) for meta data
+    temp_shared <- read.delim(paste(path_to_file, i, "/", i, ending, sep = ""), 
+                              header = T, stringsAsFactors = F)
+    
+    # conditional that checks for column named sample_type (created during mothur processing)
+    if(!("sample_type" %in% colnames(temp_shared))){
+      # Create a new column called sample_type if it is not already present
+      temp_shared <- temp_shared %>% mutate(sampleID = sample) %>% 
+        select(-sample) %>% select(sampleID, everything())
+      
+    } else{
+      # reads in data (assumes tab delimited) if sample_type present
+      temp_shared <- temp_shared %>% 
+        filter(sample_type == "stool") %>% mutate(sampleID = sample) %>% 
+        select(-sample) %>% select(sampleID, everything())
+    }
+    
+  }
+  # prints message to stdout updating on completion
+  print(paste("Completed Reading in: ", i, ending, " data", sep = ""))
+  # returns the necessary temp_shared file
+  return(temp_shared)
+}
 
 
 
