30350726954756b9416d2799ab2aa3bf01404b5c
marcsze <marcasze@gmail.com>
2017-09-06 08:48:05 -0400

    add ind RR and counts for composite analysis

--- code/get_specifc_genus_analysis_tissue.R
+++ code/get_specifc_genus_analysis_tissue.R
@@ -95,6 +95,140 @@ get_specific_genera <- function(i, genera_to_get, table_name, meta_name,
 
 
 
+# Analyze the data with respect to high low column table
+analyze_study <- function(i, group_column, vec_of_int, dataset){
+  # i represents the study 
+  # group_column represents what the case/control column is
+  # vec_of_int is a vector with genera of interest
+  # dataset is the list of combined genera of interest and meta data
+  
+  # Vector of whether sample was cancer or not
+  is_cancer <- factor(ifelse(dataset[[i]][, group_column] == "cancer", 
+                             invisible("Y"), invisible("N")), levels = c("Y", "N"))
+  # Vector of median values 
+  thresholds <- apply(select(dataset[[i]], one_of(vec_of_int)), 2, 
+                      function(x) median(x))
+  # Runs the code to generate high/low calls for the alpha metrics used based on median
+  highs_lows <- sapply(vec_of_int, 
+                       function(x) create_high_low(i = i, thresholds, 
+                                                   x, group_column, 
+                                                   dataset = dataset), simplify = F)
+
+  names(highs_lows) <- vec_of_int # forces names for the list
+  # Obtains the individual relative risk and CI for each study
+  obtained_rr <- lapply(highs_lows, 
+                        function(x) run_rr(high_low_vector = x, disease_vector = is_cancer)) 
+  return(obtained_rr) # returns a list that stores all the table counts and RR data
+}
+
+
+# Function that creates the needed high/low columns
+create_high_low <- function(i, threshold, var_of_interest, grouping, 
+                            dataset){
+  # i is the study
+  # threshold is the vector of median values for alpha measures of interest
+  # var_of_interest is the genera being used
+  # grouping is the name of the case/control column
+  # dataset is the data list you want to work with
+  
+  # get specific data table of interest based on study
+  select_data <- dataset[[i]]
+  
+  # create a vector with high/low versus the median value provided
+  high_low <- factor(ifelse(select_data[, var_of_interest] <= threshold, 
+                            invisible("low"), invisible("high")), levels = c("high", "low"))
+  # Returns the vector of high/low calls
+  return(high_low)
+}
+
+
+# Function that runs relative risk test on single variable
+run_rr <- function(high_low_vector, disease_vector){
+  # high_low_vector is the respective call columns from high_low for a specific alpha measure
+  # disease_vector is the "is_cancer" vector is case/control info
+  
+  # Creates a 2x2 table of counts
+  contingency <- table(high_low_vector, disease_vector)
+  
+  # check if there are 0 counts
+  check_values <- as.vector(contingency)
+  
+  # runs the RR test based on the obtained 2x2 table
+  test <- epi.2by2(contingency, method="cohort.count")
+  # Pull only specific information from the stored list in "test"
+  test_values <- cbind(test$massoc$RR.strata.score, 
+                       pvalue = test$massoc$chisq.strata$p.value)
+  # store both the obtained raw counts and the resulting RR with pvalue
+  combined_data <- list(data_tbl = contingency, test_values = test_values)
+  
+  # Returns a list with all information needed for downstream analysis
+  return(combined_data)
+}
+
+
+# Function to seperate out the table data from the individual analysis data
+pull_data <- function(var_of_int, i, result, datalist){
+  # var_of_int is the alpha measures used e.g. "sobs"
+  # i is the study
+  # result is the type of data we want either "test_values" or "data_tbl"
+  # datalist is defaulted to test_ind_RR to make it easier to work with mapply
+  
+  # Pull the needed data and add identifiers
+  tempData <- datalist[[i]][[var_of_int]][[result]] %>% as.data.frame() %>% 
+    mutate(measure = var_of_int, study = i)
+  # return the pulled data
+  return(tempData)
+  
+}
+
+
+# A control function to direct final table creation for ind RR analysis
+make_list <- function(i, vec_of_interest, result, datalist){
+  # i is the study
+  # result is they type of data being pulled "test_values" or "data_tbl"
+  # vec_of_interest is the genera to be analyzed
+  # datalist is defaulted to ind_study_data to make it easier to work with mapply
+  
+  # runs the function iteratively to collect the specific data
+  pulled_data <- sapply(vec_of_interest, 
+                        function(x) pull_data(i = i, x, 
+                                              result = result, 
+                                              datalist = datalist), simplify = F) %>% 
+    bind_rows()
+  
+    #pulled_data <- mapply(pull_data, vec_of_interest, 
+  #                      i, result, SIMPLIFY = F) %>% bind_rows()
+  # returns a nice data table
+  return(pulled_data)
+}
+
+
+# Function to run test for selected alpha measure
+run_pooled <- function(alpha_d, dataset = ind_counts_data){
+  # alpha_d is the alpha measure of interest
+  # dataset is defaulted to ind_counts_data
+  
+  # select only the relevent alpha measures
+  test_data <- dataset %>% filter(measure == alpha_d)
+  
+  # Run the actual pooled test
+  rr_pooled_test <- rma(ai = high_Y, bi = high_N, 
+                        ci = low_Y, di = low_N, data = test_data, 
+                        measure = "RR", method = "REML")
+  # Store a vector of the important results of interest
+  results <- c(exp(c(rr = rr_pooled_test$b[[1, 1]], ci_lb = rr_pooled_test$ci.lb, 
+                     ci_ub=rr_pooled_test$ci.ub)), pvalue = rr_pooled_test$pval, 
+               measure = alpha_d)
+  # returns the vector of results
+  return(results)
+  
+}
+
+
+
+
+
+
 ##############################################################################################
 ############### Run the actual programs to get the data ######################################
 ##############################################################################################
@@ -114,6 +248,11 @@ tissue_unmatched <- read.csv("data/process/tables/alpha_tissue_unmatched_data.cs
 
 # Manually remove the one matched polyp sample in dejea study
 tissue_matched <- tissue_matched %>% filter(id != 3776)
+
+# Create new column where polyp is assigned to control for tissue unmatched group
+tissue_unmatched <- tissue_unmatched %>% 
+  mutate(disease2 = ifelse(disease == "polyp", invisible("control"), invisible(disease)))
+
 
 # Remove polyp only group
 no_p_tissue_matched <- tissue_matched %>% filter(study != "lu")
@@ -139,6 +278,51 @@ unmatched_specific_genera_list <- sapply(
   function(x) get_specific_genera(x, crc_genera, 
                                   "sub_genera_data", "study_meta", 
                                   ind_unmatched_data), simplify = F)
+
+
+
+# Generate the RR for each respective study for each genus of interest
+# Return both counts and results
+matched_test_ind_RR <- sapply(
+  c("burns", "dejea", "geng"), 
+  function(x) analyze_study(x, "disease", 
+                            crc_genera, matched_specific_genera_list), simplify = F)
+
+
+unmatched_test_ind_RR <- sapply(
+  c("burns", "sana", both_sets), 
+  function(x) analyze_study(x, "disease2", 
+                            crc_genera, unmatched_specific_genera_list), simplify = F)
+
+
+# Store the results from the individual testing here
+matched_RR_data <- sapply(c("burns", "dejea", "geng"), 
+                      function(x) make_list(x, crc_genera, "test_values", matched_test_ind_RR), 
+                      simplify = F) %>% bind_rows()
+
+unmatched_RR_data <- sapply(c("burns", "sana", both_sets), 
+                          function(x) make_list(x, crc_genera, "test_values", unmatched_test_ind_RR), 
+                          simplify = F) %>% bind_rows()
+
+
+# Store the counts and rearrange the table to be used in the pooled analysis
+matched_counts_data <- sapply(c("burns", "dejea", "geng"), 
+                          function(x) make_list(x, crc_genera, "data_tbl", 
+                                                matched_test_ind_RR), simplify = F) %>% 
+  bind_rows() %>% unite(group, high_low_vector, disease_vector, sep = "_") %>% 
+  spread(group, Freq)
+
+
+
+unmatched_counts_data <- sapply(c("burns", "sana", both_sets), 
+                              function(x) make_list(x, crc_genera, "data_tbl", 
+                                                    unmatched_test_ind_RR), simplify = F) %>% 
+  bind_rows() %>% unite(group, high_low_vector, disease_vector, sep = "_") %>% 
+  spread(group, Freq)
+
+
+
+
 
 
 
