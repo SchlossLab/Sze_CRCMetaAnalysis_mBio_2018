c3e8ffeab1e7edbdc4943c07fb101efe149ec20d
marcsze <marcasze@gmail.com>
2017-09-07 23:56:16 -0400

    first commit

---
+++ code/run_random_forest_genus_stool.R
@@ -0,0 +1,137 @@
+### Run Random Forest Analysis
+### Generate model and then test on remaining studies
+### Marc Sze
+
+# Load in needed functions and libraries
+source('code/functions.R')
+
+# Load needed libraries
+loadLibs(c("dplyr", "tidyr", "epiR", "metafor"))
+
+# Tissue Only sets
+# Lu, Dejea, Sana, Burns, Geng
+# Remove Lu since it only has polyps and no cancer cases
+tissue_sets <- c("dejea", "sana", "burns", "geng")
+
+# Stool Only sets
+# Hale, Wang, Brim, Weir, Ahn, Zeller, Baxter
+# Ignore brim since it only has polyps
+stool_sets <- c("wang", "weir", "ahn", "zeller", "baxter", "hale")
+
+# Both Tissue and Stool
+# flemer sampletype = biopsy or stool
+# chen sample_type = tissue or stool
+# Flemer, Chen
+# Ignore chen for stool since there is only one case
+both_sets <- c("chen", "flemer")
+
+
+# Control function to get all the data, basically runs the above functions in a
+# contained location withouth having to repeat them
+get_data <- function(i){
+  # i is the study of interest
+  
+  # gets original sample names
+  sample_names <- (get_file(i, "data/process/", "_genera_shared.csv") %>% 
+                     mutate(sample_names = rownames(.)) %>% select(sample_names))[, "sample_names"]
+  # grabs subsampled data and assigns rownames from sample names to table
+  sub_genera_data <- get_file(i, "data/process/", "_subsample_genera.csv", rows_present = F, 
+                              vec_of_rownames = sample_names) %>% 
+    as.data.frame() %>% mutate(sample_ID = rownames(.)) %>% 
+    select(sample_ID, everything())
+  # grabs the meta data and transforms polyp to control (polyp/control vs cancer) 
+  study_meta <- get_file(i, "data/process/", ".metadata", rows_present = F,  
+                         "stool", metadata = T)
+  # conditional that checks for whether length of rows of meta data is smaller
+  if(length(rownames(study_meta)) < length(rownames(sub_genera_data))){
+    # grab only the samples in the meta data file for down stream analysis
+    sub_genera_data <- sub_genera_data %>% slice(match(study_meta$sampleID, sample_ID))
+    
+  } else{
+    # grab only files in the data file for analysis
+    study_meta <- study_meta %>% slice(match(sub_genera_data$sample_ID, sampleID))
+  }
+  
+  print(paste("Total number of columns in", i, "is", 
+              length(colnames(sub_genera_data))))
+  # re assigns the rown names while removing the extra column used for sorting
+  sample_names <- sub_genera_data$sample_ID
+  # creates a list file with both data sets
+  dataList <- list(sub_genera_data = sub_genera_data, 
+                   study_meta = study_meta, 
+                   column_length = length(colnames(sub_genera_data)))
+  # returns the combined list file
+  return(dataList)
+  
+}
+
+
+# Function that aligns the genera from all data sets
+align_genera <- function(studies, length_column_name, 
+                         genera_data_name, dataList){
+  
+  genera_num_list <- sort.int(sapply(studies, 
+                            function(x) dataList[[x]][[length_column_name]]))
+  
+  lowest_genera_study <- names(genera_num_list[1])
+  
+  genera_names <- dataList[[lowest_genera_study]][[genera_data_name]] %>% 
+    select(-contains("_unclassified")) %>% colnames(.)
+  
+  
+  temp_aligned_genera <- sapply(studies, 
+                           function(x) 
+                             select(dataList[[x]][[genera_data_name]], 
+                                    one_of(genera_names)), simplify = F)
+  
+  new_num_list <- sort.int(sapply(studies, 
+                                     function(x) 
+                                       length(colnames(temp_aligned_genera[[x]]))))
+  
+  rd2_lowest_genera_study <- names(new_num_list[1])
+  
+  genera_names <- colnames(temp_aligned_genera[[rd2_lowest_genera_study]])
+  
+  
+  temp2_aligned_genera <- sapply(studies, 
+                                function(x) 
+                                  select(temp_aligned_genera[[x]],  
+                                         one_of(genera_names)), simplify = F)
+  
+  
+  new_num_list2 <- sort.int(sapply(studies, 
+                                  function(x) 
+                                    length(colnames(temp2_aligned_genera[[x]]))))
+  
+  rd3_lowest_genera_study <- names(new_num_list2[1])
+  
+  genera2_names <- colnames(temp2_aligned_genera[[rd3_lowest_genera_study]])
+  
+  
+  temp3_aligned_genera <- sapply(studies, 
+                                 function(x) 
+                                   select(temp2_aligned_genera[[x]],  
+                                          one_of(genera2_names)), simplify = F)
+  
+  
+  return(temp3_aligned_genera)
+}
+
+
+
+
+
+##############################################################################################
+############### Run the actual programs to get the data ######################################
+##############################################################################################
+
+# reads in all the stool data into one list
+stool_study_data <- mapply(get_data, c(stool_sets, "flemer"), SIMPLIFY = F)
+
+
+test <- align_genera(stool_sets, "column_length", "sub_genera_data", stool_study_data)
+
+test2 <- align_genera(stool_sets, "column_length", "sub_genera_data", test)
+
+
+
