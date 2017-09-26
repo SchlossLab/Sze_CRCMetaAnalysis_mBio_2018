23c9215b3fc7bb14b8f887c11e68fa76631f54a1
marcsze <marcasze@gmail.com>
2017-09-26 08:40:33 -0400

    add read in of data

--- code/run_random_forest_genus_tissue.R
+++ code/run_random_forest_genus_tissue.R
@@ -8,35 +8,79 @@ source('code/functions.R')
 # Load needed libraries
 loadLibs(c("dplyr", "tidyr", "caret", "pROC"))
 
-# Tissue Only sets
-# Lu, Dejea, Sana, Burns, Geng
-# Remove Lu since it only has polyps and no cancer cases
-tissue_sets <- c("dejea", "sana", "burns", "geng")
-
-# Stool Only sets
-# Hale, Wang, Brim, Weir, Ahn, Zeller, Baxter
-# Ignore brim since it only has polyps
-stool_sets <- c("wang", "weir", "ahn", "zeller", "baxter", "hale")
-
-# Both Tissue and Stool
-# flemer sampletype = biopsy or stool
-# chen sample_type = tissue or stool
-# Flemer, Chen
-# Ignore chen for stool since there is only one case
-both_sets <- c("chen", "flemer")
-
-
-
 # Read in specific data tables to be used
 
 tissue_matched <- read.csv("data/process/tables/alpha_tissue_matched_data.csv", 
                            header = T, stringsAsFactors = F) %>% 
   mutate(matchings = ifelse(disease == "cancer" | disease == "polyp", 1, 0)) %>% 
-  filter(id != "3776") # remove polyp sample
+  # remove polyp sample
+  filter(id != "3776") %>% 
+  select(group, id, disease, study) %>% 
+  rename(sample_id = group)
+  
 
+  
 tissue_unmatched <- read.csv("data/process/tables/alpha_tissue_unmatched_data.csv", 
                              header = T, stringsAsFactors = F) %>% 
-  mutate(disease = gsub("adenoma", "polyp", disease))
+  mutate(disease = gsub("adenoma", "polyp", disease)) %>% 
+  select(group, id, disease, study) %>% 
+  rename(sample_id = group)
+
+
+# Get matched studies
+matched_studies <- unique(tissue_matched$study)
+unmatched_studies <- unique(tissue_unmatched$study)
+
+
+
+
+
+##############################################################################################
+########################## Group of Functions needed to run the analysis #####################
+##############################################################################################
+
+
+# Control function to get all the data, basically runs the above functions in a
+# contained location withouth having to repeat them
+get_data <- function(i, metadata){
+  # i is the study of interest
+  
+  # gets original sample names
+  sample_names <- (get_file(i, "data/process/", "_genera_shared.csv") %>% 
+                     mutate(sample_names = rownames(.)) %>% select(sample_names))[, "sample_names"]
+  # grabs subsampled data and assigns rownames from sample names to table
+  sub_genera_data <- get_file(i, "data/process/", "_subsample_genera.csv", rows_present = F, 
+                              vec_of_rownames = sample_names) %>% 
+    as.data.frame() %>% mutate(sample_ID = rownames(.)) %>% 
+    select(sample_ID, everything())
+  # grabs the meta data and transforms polyp to control (polyp/control vs cancer) 
+  study_meta <- metadata %>% filter(study == i)
+    
+  # Looks for Na in the meta data of interest and removes respective samples
+  study_meta <- study_meta %>% filter(!is.na(disease))
+  
+  # conditional that checks for whether length of rows of meta data is smaller
+  if(length(rownames(study_meta)) < length(rownames(sub_genera_data))){
+    # grab only the samples in the meta data file for down stream analysis
+    sub_genera_data <- sub_genera_data %>% slice(match(study_meta$sample_id, sample_ID))
+    
+  } else{
+    # grab only files in the data file for analysis
+    study_meta <- study_meta %>% slice(match(sub_genera_data$sample_ID, sample_id))
+  }
+  # Prints out the total number of genera for that specific study
+  print(paste("Total number of columns in", i, "is", 
+              length(colnames(sub_genera_data))))
+  # re assigns the rown names while removing the extra column used for sorting
+  sample_names <- sub_genera_data$sample_ID
+  # creates a list file with both data sets
+  dataList <- list(sub_genera_data = sub_genera_data, 
+                   study_meta = study_meta, 
+                   column_length = length(colnames(sub_genera_data)))
+  # returns the combined list file
+  return(dataList)
+  
+}
 
 
 
@@ -54,13 +98,14 @@ tissue_unmatched <- read.csv("data/process/tables/alpha_tissue_unmatched_data.cs
 
 
 
+##############################################################################################
+########################## Code used to run the analysis (unmatched) #########################
+##############################################################################################
 
 
-
-
-
-
-
+# reads in all the stool data into one list
+stool_study_data <- sapply(unmatched_studies, 
+                           function(x) get_data(x, tissue_unmatched), simplify = F)
 
 
 
