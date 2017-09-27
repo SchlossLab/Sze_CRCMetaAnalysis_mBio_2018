d57950393611c1409839ed962381954db0913972
marcsze <marcasze@gmail.com>
2017-09-27 14:51:46 -0400

    add comments to code

--- code/run_random_forest_otu_stool.R
+++ code/run_random_forest_otu_stool.R
@@ -112,9 +112,9 @@ get_align_info <- function(datatable){
   training_data <- datatable %>% select(-disease, -random_disease)
   # Check for columns that have near zero variance
   nzv <- nearZeroVar(training_data)
-  
+  # check to see if at least one value has near zero variance
   if(length(nzv) == 0){
-    
+    # No nzv then assign training data to be itself
     training_data <- training_data
   } else{
     
@@ -138,13 +138,15 @@ get_align_info <- function(datatable){
 }
 
 
-# Function that will run and create the needed model
+# Function that will run and create the needed models
 make_rf_model <- function(run_marker, study, train_data){
+  # run_marker is the model iteration that has currently completed
+  # study is a vector for the data set to be used
   # train_data is the data table to be used for model training
   
-  # Create a LOOCV if data set is small (manually specify)
+  # Create a smaller cross-validation amount based on small n present
   if(study %in% c("weir")){
-    
+    # type of training method to be used (cv stands for cross-validation)
     method_used <- "cv"
     
     #Create Overall specifications for model tuning
@@ -158,9 +160,9 @@ make_rf_model <- function(run_marker, study, train_data){
       classProbs = TRUE, 
       summaryFunction = twoClassSummary, 
       savePredictions = "final")
-    
+  # Uses a default 10 fold cross validation if n is not small  
   } else{
-    
+    # type of method to be used (cv stands for cross-validation)
     method_used <- "cv"
     
     #Create Overall specifications for model tuning
@@ -206,17 +208,23 @@ make_rf_model <- function(run_marker, study, train_data){
 
 
 # Function to get the min and max models to generate roc curves for
+# actual and random models.
 get_min_max <- function(a_models, r_models, a_summary, r_summary){
+  # a_models is an object with the actual models
+  # r_models is an object with the random models
+  # a_summary is a table with the actual model information for each run (e.g. AUC, sens, etc.)
+  # r_summary is a table with the random model information for each run (e.g. AUC, sens, etc.)
   
+  # get the min and max AUC for the actual model
   a_min_row <- as.numeric((a_summary %>% filter(ROC == min(ROC)) %>% select(runs))[, "runs"])
   a_max_row <- as.numeric((a_summary %>% filter(ROC == max(ROC)) %>% select(runs))[, "runs"])
-  
+  # Get the min and max AUC for the random model
   r_min_row <- as.numeric((r_summary %>% filter(ROC == min(ROC)) %>% select(runs))[, "runs"])
   r_max_row <- as.numeric((r_summary %>% filter(ROC == max(ROC)) %>% select(runs))[, "runs"])
-  
+  # Check to see if there are more than one option for best or worse AUC
   if(length(r_max_row) > 1 | length(r_min_row) > 1 | 
      length(a_max_row) > 1 | length(a_min_row) > 1){
-    
+    # Takes the first choice if there are multiple to choose from
     a_min_row <- a_min_row[1]
     a_max_row <- a_max_row[1]
     r_min_row <- r_min_row[1]
@@ -224,7 +232,8 @@ get_min_max <- function(a_models, r_models, a_summary, r_summary){
     
   }
   
-  
+  # Create a summary list with the best and worst model information 
+  # for the actual and random models
   tempList <- list(
     actual_mod = list(
       min_model = a_models[[a_min_row]], 
@@ -232,8 +241,7 @@ get_min_max <- function(a_models, r_models, a_summary, r_summary){
     random_mod = list(
       min_model = r_models[[r_min_row]], 
       max_model = r_models[[r_max_row]]))
-  
-  
+  # Return the variable to the global work environment
   return(tempList)
 }
 
@@ -242,20 +250,28 @@ get_min_max <- function(a_models, r_models, a_summary, r_summary){
 # Function that generates ROC curves and then compares them to random
 make_summary_data <- function(i, model_info, dataList, a_summary, r_summary,  
                               train_name, random_name){
+  # i is the study variable
+  # model_info is a list with the best and worst models for the actual and random models
+  # dataList is the list that contains the acutal data that was used to create the models
+  # a_summary is a table with the actual model information for each run (e.g. AUC, sens, etc.)
+  # r_summary is a table with the random model information for each run (e.g. AUC, sens, etc.)
+  # train_name is the name of the training set
+  # random_name is the name of the random set
   
+  # Generate the best and worst roc curves for the actual model
   best_actual_roc <- roc(dataList[[train_name]]$disease ~ 
                            model_info[["actual_mod"]][["max_model"]][["pred"]][, "cancer"])
   worst_actual_roc <- roc(dataList[[train_name]]$disease ~ 
                             model_info[["actual_mod"]][["min_model"]][["pred"]][, "cancer"])
   
-  
+  # Generate the best and worst roc curves for the random model
   best_random_roc <- roc(dataList[[random_name]]$disease ~ 
                            model_info[["random_mod"]][["max_model"]][["pred"]][, "cancer"])
   worst_random_roc <- roc(dataList[[random_name]]$disease ~ 
                            model_info[["random_mod"]][["min_model"]][["pred"]][, "cancer"])
-  
+  # Generate a p-value on whether the distribution between actual and random are different
   pvalue <- t.test(a_summary$ROC, r_summary$ROC)$p.value
-  
+  # Create a final list with all the needed ROC curve data and respective p-value
   finalData <- list(
     all_data = cbind(
       sens = c(best_actual_roc$sensitivities, worst_actual_roc$sensitivities, 
@@ -274,8 +290,7 @@ make_summary_data <- function(i, model_info, dataList, a_summary, r_summary,
       mutate(sens = as.numeric(sens), spec = as.numeric(spec), 
              study = rep(i, length(spec))), 
     pvalue = pvalue)
-  
-
+  # Return the final list back to the global work environment
   return(finalData)
   
 }
@@ -290,41 +305,41 @@ make_summary_data <- function(i, model_info, dataList, a_summary, r_summary,
 all_roc_data <- NULL
 all_comparisons <- NULL
 
-# Set up direction variables
+# Set up direction variables to set number of models to run
 actual_runs <- paste("act_model_", seq(1:100), sep = "")
 random_runs <- paste("rand_model_", seq(1:100), sep = "")
 
 # Iteratively run through each study for stool
 for(i in c(stool_sets, "flemer")){
-  
+  # Gets the respective data
   dataList <- get_data(i = i)
-  
+  # merges the needed metadata with the variables to test and creates a random label as well
   disease_dataset <- assign_disease("study_meta", "shared_data", dataList)
-  
+  # makes sure all the genera are the same for every data set to be tested
   rf_data <- get_align_info(disease_dataset)
-  
+  # generates a distribution of models based on real data
   actual_model <- sapply(actual_runs, 
                          function(x) make_rf_model(x, i, rf_data[["train_data"]]), simplify = F) 
-  
+  # generates a distribution of models based on random labeleed data
   random_model <- sapply(random_runs, 
                          function(x) make_rf_model(x, i, rf_data[["rand_data"]]), simplify = F)
-  
+  # create a data table with summary stats of all n actual models run
   actual_summary <- sapply(actual_model, 
                            function(x) x$results, simplify = F) %>% bind_rows() %>% 
     mutate(runs = rownames(.))
-  
+  # create a data table with summary stats of all n random models run
   random_summary <- sapply(random_model, 
                            function(x) x$results, simplify = F) %>% bind_rows() %>% 
     mutate(runs = rownames(.))
-  
+  # Generates the best and worst models for actual and random models
   model_info <- get_min_max(actual_model, random_model, 
                       actual_summary, random_summary)
-  
+  # Gets the pvalue between actual and random as well as needed graphing data
   test <- make_summary_data(i = i, model_info = model_info, rf_data, 
                             actual_summary, random_summary, "train_data", "rand_data")
-  
+  # transforms the collected roc data into a data frame
   all_roc_data <- all_roc_data %>% bind_rows(test[["all_data"]])
-  
+  # Creates a summary table for the comparisons made
   all_comparisons <- rbind(all_comparisons, 
                            as.data.frame.list(
                              c(actual_summary %>% summarise(act_mean_auc = mean(ROC, na.rm = T), 
@@ -332,11 +347,10 @@ for(i in c(stool_sets, "flemer")){
                              random_summary %>% summarise(rand_mean_auc = mean(ROC, na.rm = T), 
                                                           rand_sd_auc = sd(ROC, na.rm = T)), 
                              pvalue = test[["pvalue"]], study = i)))
-  
+  # Tracking print out that allows tracking of which study has completed
   print(paste("Completed study:", i, "RF testing"))
   
 }
-
 
 
 # Write out the relevant data frames
