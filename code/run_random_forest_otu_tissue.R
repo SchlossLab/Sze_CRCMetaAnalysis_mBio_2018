740d61d4bd7d023028debd429648e12ab5b7f0e8
marcsze <marcasze@gmail.com>
2017-09-27 08:18:31 -0400

    add data read in function

--- code/run_random_forest_otu_tissue.R
+++ code/run_random_forest_otu_tissue.R
@@ -42,5 +42,70 @@ unmatched_studies <- unique(
   tissue_unmatched$study[!(tissue_unmatched$study %in% c("dejea", "lu"))]) 
 
 
+##############################################################################################
+########################## Group of Functions needed to run the analysis #####################
+##############################################################################################
+
+
+# Control function to get all the data, basically runs the above functions in a
+# contained location withouth having to repeat them
+get_data <- function(i, metadata){
+  # i is the study of interest
+  
+  # grabs subsampled data and assigns rownames from sample names to table
+  shared_data <- read.delim(paste("data/process/", i, "/", i, ".0.03.subsample.shared", 
+                                  sep = ""), header = T, stringsAsFactors = F) %>% 
+    select(-label, -numOtus)
+  # grabs the meta data and transforms polyp to control (polyp/control vs cancer) 
+  study_meta <- metadata %>% filter(study == i)
+  
+  # Looks for Na in the meta data of interest and removes respective samples
+  study_meta <- study_meta %>% filter(!is.na(disease))
+  
+  # conditional that checks for whether length of rows of meta data is smaller
+  if(length(rownames(study_meta)) < length(rownames(shared_data))){
+    # grab only the samples in the meta data file for down stream analysis
+    shared_data <- shared_data %>% slice(match(study_meta$sample_id, Group))
+    
+  } else{
+    # grab only files in the data file for analysis
+    study_meta <- study_meta %>% slice(match(shared_data$Group, sample_id))
+  }
+  # Prints out the total number of genera for that specific study
+  print(paste("Total number of columns in", i, "is", 
+              length(colnames(shared_data))))
+  # creates a list file with both data sets
+  dataList <- list(shared_data = shared_data, 
+                   study_meta = study_meta, 
+                   column_length = length(colnames(shared_data)))
+  # returns the combined list file
+  return(dataList)
+  
+}
+
+
+##############################################################################################
+########################## Code used to run the analysis (unmatched) #########################
+##############################################################################################
+
+
+
+
+
+##############################################################################################
+########################## Code used to run the analysis (unmatched) #########################
+##############################################################################################
+
+for(i in matched_studies){
+  
+  dataList <- get_data(i = i, tissue_matched)
+  
+}
+
+
+
+
+
+
 
 
