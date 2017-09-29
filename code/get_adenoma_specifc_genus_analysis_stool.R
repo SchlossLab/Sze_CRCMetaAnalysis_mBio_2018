d7844621b1a38c448253f00e6be944f93faa35ae
marcsze <marcasze@gmail.com>
2017-09-29 07:54:59 -0400

    first commit

---
+++ code/get_adenoma_specifc_genus_analysis_stool.R
@@ -0,0 +1,49 @@
+### Pull, transform, and normalize 4 crc genera - Adenoma
+### Specifically analyze the genera tied to crc from previous research
+### Marc Sze
+
+# Load in needed functions and libraries
+source('code/functions.R')
+
+# Load needed libraries
+loadLibs(c("dplyr", "tidyr", "epiR", "metafor"))
+
+
+# Stool Only polyp sets
+# Hale, Wang, Brim, Weir, Ahn, Zeller, Baxter
+stool_sets <- c("brim", "zeller", "baxter", "hale")
+
+
+
+
+##############################################################################################
+############### List of functions used to analyze the data ###################################
+##############################################################################################
+
+
+
+
+
+
+
+
+
+##############################################################################################
+############### Run the actual programs to get the data ######################################
+##############################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
