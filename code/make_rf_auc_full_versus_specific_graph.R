39f8792ccc9073ea9b592e778f637f33f2e9b053
marcsze <marcasze@gmail.com>
2017-10-19 08:47:09 -0400

    add data read in component

--- code/make_rf_auc_full_versus_specific_graph.R
+++ code/make_rf_auc_full_versus_specific_graph.R
@@ -8,3 +8,18 @@ source("code/functions.R")
 loadLibs(c("tidyverse", "gridExtra", "viridis"))
 
 
+# Load needed data tables (adenoma)
+adn_tissue_matched <- 
+  read_csv("data/process/tables/adn_genus_matched_tissue_RF_fullvsselect_pvalue_summary.csv")
+adn_tissue_unmatched <- 
+  read_csv("data/process/tables/adn_genus_unmatched_tissue_RF_fullvsselect_pvalue_summary.csv")
+
+adn_all_stool <- read_csv("data/process/tables/adn_genus_stool_RF_fullvsselect_pvalue_summary.csv")
+
+# Load in needed data tables (carcinoma)
+crc_tissue_matched <- 
+  read_csv("data/process/tables/genus_matched_tissue_RF_fullvsselect_pvalue_summary.csv")
+crc_tissue_unmatched <- 
+  read_csv("data/process/tables/genus_unmatched_tissue_RF_fullvsselect_pvalue_summary.csv")
+
+crc_all_stool <- read_csv("data/process/tables/genus_stool_RF_fullvsselect_pvalue_summary.csv")
