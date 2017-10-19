1bf5866fa72a6f1eec6b3392644b8cae0b27feae
marcsze <marcasze@gmail.com>
2017-10-19 08:29:23 -0400

    first commit

---
+++ code/make_rf_auc_full_versus_specific_graph.R
@@ -0,0 +1,10 @@
+### Code to measure AUCs of full data set and select genera dataset
+### Marc Sze
+
+# Load in needed generic functions
+source("code/functions.R")
+
+# Load in needed libraries
+loadLibs(c("tidyverse", "gridExtra", "viridis"))
+
+
