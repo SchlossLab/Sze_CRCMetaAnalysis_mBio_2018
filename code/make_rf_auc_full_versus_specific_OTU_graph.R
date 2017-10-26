6c8ce32801910817faffe71fccc99d002a28fe12
marcsze <marcasze@gmail.com>
2017-10-26 08:36:16 -0400

    add full graphs

--- code/make_rf_auc_full_versus_specific_OTU_graph.R
+++ code/make_rf_auc_full_versus_specific_OTU_graph.R
@@ -34,20 +34,37 @@ crc_tissue_matched <- read_csv("data/process/tables/matched_tissue_rf_otu_random
   mutate(type = "matched", model_type = "full") %>% 
   bind_rows(read_csv("data/process/tables/matched_tissue_rf_select_otu_random_comparison_summary.csv") %>% 
               mutate(type = "matched", model_type = "select")) %>% 
-  gather(key = measure_type, value = AUC, act_mean_auc, rand_mean_auc)
+  gather(key = measure_type, value = AUC, act_mean_auc, rand_mean_auc, act_sd_auc, rand_sd_auc) %>% 
+  separate(measure_type, c("model", "measure_type", "X1")) %>% 
+  select(-X1) %>% 
+  spread(key = measure_type, value = AUC) %>% 
+  rename(AUC = mean, deviation = sd) %>% 
+  unite(combined_study, study, type, sep = "_")
 
 crc_tissue_unmatched <- 
   read_csv("data/process/tables/unmatched_tissue_rf_otu_random_comparison_summary.csv") %>% 
   mutate(type = "unmatched", model_type = "full") %>% 
   bind_rows(read_csv("data/process/tables/unmatched_tissue_rf_select_otu_random_comparison_summary.csv") %>% 
               mutate(type = "unmatched", model_type = "select")) %>% 
-  gather(key = measure_type, value = AUC, act_mean_auc, rand_mean_auc)
+  gather(key = measure_type, value = AUC, act_mean_auc, rand_mean_auc, act_sd_auc, rand_sd_auc) %>% 
+  separate(measure_type, c("model", "measure_type", "X1")) %>% 
+  select(-X1) %>% 
+  spread(key = measure_type, value = AUC) %>% 
+  rename(AUC = mean, deviation = sd) %>% 
+  unite(combined_study, study, type, sep = "_")
+
+crc_all_tissue <- crc_tissue_matched %>% bind_rows(crc_tissue_unmatched)
+
 
 crc_all_stool <- read_csv("data/process/tables/stool_rf_otu_random_comparison_summary.csv") %>% 
   mutate(model_type = "full") %>% 
   bind_rows(read_csv("data/process/tables/stool_rf_select_otu_random_comparison_summary.csv") %>% 
               mutate(model_type = "select")) %>% 
-  gather(key = measure_type, value = AUC, act_mean_auc, rand_mean_auc)
+  gather(key = measure_type, value = AUC, act_mean_auc, rand_mean_auc, act_sd_auc, rand_sd_auc) %>% 
+  separate(measure_type, c("model", "measure_type", "X1")) %>% 
+  select(-X1) %>% 
+  spread(key = measure_type, value = AUC) %>% 
+  rename(AUC = mean, deviation = sd)
 
 
 
@@ -89,11 +106,11 @@ adn_tissue_graph <- adn_tissue %>%
   geom_errorbar(aes(ymin = AUC-deviation, ymax = AUC+deviation), 
                 width = 0.25, size = 0.7, show.legend = F) + 
   geom_hline(yintercept = 0.5, linetype = "dashed") + 
-  facet_grid(. ~ model_type) + coord_cartesian(ylim = c(0, 1.05)) + 
+  facet_grid(. ~ model_type) + coord_flip(ylim = c(0, 1.05)) + 
   labs(x = "", y = "Model AUC") + theme_bw() + ggtitle("A") + 
   scale_color_manual(name = "Study", 
                      values = c('#440154FF', '#FDE725FF')) + 
-  annotate("text", label = paste("Adenoma"), x = 0.6, y = 1.07, size = 2.5) + 
+  annotate("text", label = paste("Adenoma"), x = 2.55, y = 0.04, size = 2.5) + 
   theme(plot.title = element_text(face="bold", hjust = -0.1, size = 20), 
         legend.text = element_text(size = 6), 
         panel.grid.major = element_blank(), 
@@ -114,19 +131,92 @@ adn_stool_graph <- adn_all_stool %>%
   geom_errorbar(aes(ymin = AUC-deviation, ymax = AUC+deviation), 
                 width = 0.25, size = 0.7, show.legend = F) + 
   geom_hline(yintercept = 0.5, linetype = "dashed") + 
-  coord_cartesian(ylim = c(0, 1.05)) + 
+  coord_flip(ylim = c(0, 1.05)) + 
   facet_grid(. ~ model_type) + 
   labs(x = "", y = "Model AUC") + theme_bw() + ggtitle("A") + 
   scale_color_manual(name = "Study", 
                      values = c('#481D6FFF', '#1F998AFF', '#67CC5CFF', '#FDE725FF')) + 
-  annotate("text", label = paste("Adenoma (Stool)"), x = 0.80, y = 1.09, size = 2.5) + 
+  annotate("text", label = paste("Adenoma (Stool)"), x = 4.5, y = 0.1, size = 2.5) + 
   theme(plot.title = element_text(face="bold", hjust = -0.25, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         axis.text.y = element_text(size = 10))
 
 
+
+crc_tissue_graph <- crc_all_tissue %>% 
+  mutate(model_type = factor(model_type, 
+                             levels = c("full", "select"), 
+                             labels = c("All Genera", "CRC Associated\nGenera Only")), 
+         combined_study = factor(combined_study, 
+                        levels = c("sana_unmatched", "geng_matched", "flemer_unmatched", 
+                                   "dejea_matched", "chen_unmatched", "burns_unmatched",
+                                   "burns_matched"), 
+                        labels = c("Sanapareddy", "Geng\n(matched)", "Flemer", "Dejea\n(matched)", 
+                                   "Chen", "Burns\n(matched)", "Burns"))) %>% 
+  filter(grepl("rand", model) != T) %>% 
+  ggplot(aes(combined_study, AUC, color = combined_study, group = combined_study)) + 
+  geom_point(size = 3.5, show.legend = F) + 
+  geom_errorbar(aes(ymin = AUC-deviation, ymax = AUC+deviation), 
+                width = 0.25, size = 0.7, show.legend = F) + 
+  geom_hline(yintercept = 0.5, linetype = "dashed") + 
+  facet_grid(. ~ model_type) + coord_flip(ylim = c(0, 1.05)) + 
+  labs(x = "", y = "Model AUC") + theme_bw() + ggtitle("B") + 
+  scale_color_manual(name = "Study", 
+                     values = c('#1F998AFF', '#CBE11EFF', '#440154FF', '#2B748EFF', 
+                                '#3D4D8AFF', '#453581FF', '#453581FF')) + 
+  annotate("text", label = paste("Cancer"), x = 0.6, y = 1.07, size = 2.5) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.1, size = 20), 
+        legend.text = element_text(size = 6), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.text.y = element_text(size = 10))
+
+
+
+crc_all_stool %>% 
+  mutate(model_type = factor(model_type, 
+                             levels = c("full", "select"), 
+                             labels = c("All Genera", "CRC Associated\nGenera Only")), 
+         study = factor(study, 
+                        levels = c("zeller", "weir", "wang", "hale", "flemer", "baxter", "ahn"), 
+                        labels = c("Zeller", "Weir", "Wang", "Hale", "Flemer", "Baxter", "Ahn"))) %>% 
+  filter(grepl("rand", model) != T) %>% 
+  ggplot(aes(study, AUC, color = study, group = study)) + 
+  geom_point(size = 3.5, show.legend = F) + 
+  geom_errorbar(aes(ymin = AUC-deviation, ymax = AUC+deviation), 
+                width = 0.25, size = 0.7, show.legend = F) + 
+  geom_hline(yintercept = 0.5, linetype = "dashed") + 
+  coord_flip(ylim = c(0, 1.05)) + 
+  facet_grid(. ~ model_type) + 
+  labs(x = "", y = "Model AUC") + theme_bw() + ggtitle("B") + 
+  scale_color_manual(name = "Study", 
+                     values = c('#FDE725FF', '#24878EFF', '#97D83FFF', '#67CC5CFF', 
+                                '#440154FF', '#481D6FFF', '#40BC72FF')) + 
+  annotate("text", label = paste("Cancer (Stool)"), x = 7.45, y = 0.08, size = 2.5) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.25, size = 20), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.text.y = element_text(size = 10))
+
+
+
 ##############################################################################################
 ############### Run the actual programs to make the figure ###################################
 ##############################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
