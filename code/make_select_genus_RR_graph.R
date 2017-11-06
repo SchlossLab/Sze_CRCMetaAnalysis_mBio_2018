03df7fbae651bac5fa03a68f73540304f8f3acfc
marcsze <marcasze@gmail.com>
2017-11-06 15:47:43 -0500

    update fig 3 graph and code

--- code/make_select_genus_RR_graph.R
+++ code/make_select_genus_RR_graph.R
@@ -11,24 +11,38 @@ loadLibs(c("tidyverse", "gridExtra", "viridis"))
 adn_all_stool <- read_csv("data/process/tables/adn_select_genus_RR_stool_composite.csv") %>% 
   rename(est = rr, lower = ci_lb, upper = ci_ub) %>% 
   mutate(study = "composite") %>% 
-  bind_rows(read_csv("data/process/tables/adn_select_genus_RR_stool_ind_results.csv"))
+  bind_rows(read_csv("data/process/tables/adn_select_genus_RR_stool_ind_results.csv")) %>% 
+  bind_rows(read_csv("data/process/tables/adn_select_genus_inc_4_stool.csv") %>% 
+              rename(est = rr, lower = ci_lb, upper = ci_ub)) %>% 
+  mutate(study = ifelse(is.na(study), invisible("composite"), invisible(study)))
 
 adn_all_tissue <- read_csv("data/process/tables/adn_select_genus_RR_tissue_composite.csv") %>% 
   rename(est = rr, lower = ci_lb, upper = ci_ub) %>% 
   mutate(study = "composite") %>% 
-  bind_rows(read_csv("data/process/tables/adn_select_genus_RR_tissue_ind_results.csv"))
+  bind_rows(read_csv("data/process/tables/adn_select_genus_RR_tissue_ind_results.csv")) %>% 
+  bind_rows(read_csv("data/process/tables/adn_select_genus_inc_4_tissue.csv") %>% 
+              rename(est = rr, lower = ci_lb, upper = ci_ub)) %>% 
+  mutate(study = ifelse(is.na(study), invisible("composite"), invisible(study)))
 
 # Load in needed data tables (carcinoma)
 crc_all_stool <- read_csv("data/process/tables/select_genus_RR_stool_composite.csv") %>% 
   rename(est = rr, lower = ci_lb, upper = ci_ub) %>% 
   mutate(study = "composite") %>% 
-  bind_rows(read_csv("data/process/tables/select_genus_RR_stool_ind_results.csv"))
+  bind_rows(read_csv("data/process/tables/select_genus_RR_stool_ind_results.csv")) %>% 
+  bind_rows(read_csv("data/process/tables/select_genus_inc_4_stool.csv") %>% 
+              rename(est = rr, lower = ci_lb, upper = ci_ub)) %>% 
+  mutate(study = ifelse(is.na(study), invisible("composite"), invisible(study))) %>% 
+  filter(!(measure %in% c("one_or_more", "two_or_more", "three_or_more", "four_only")))
 
 
 crc_all_tissue <- read_csv("data/process/tables/select_genus_RR_tissue_composite.csv") %>% 
   rename(est = rr, lower = ci_lb, upper = ci_ub) %>% 
   mutate(study = "composite") %>% 
-  bind_rows(read_csv("data/process/tables/select_genus_RR_tissue_ind_results.csv"))
+  bind_rows(read_csv("data/process/tables/select_genus_RR_tissue_ind_results.csv")) %>% 
+  bind_rows(read_csv("data/process/tables/select_genus_inc_4_tissue.csv") %>% 
+              rename(est = rr, lower = ci_lb, upper = ci_ub)) %>% 
+  mutate(study = ifelse(is.na(study), invisible("composite"), invisible(study))) %>% 
+  filter(!(measure %in% c("one_or_more", "two_or_more", "three_or_more", "four_only")))
 
 
 ##############################################################################################
@@ -36,12 +50,13 @@ crc_all_tissue <- read_csv("data/process/tables/select_genus_RR_tissue_composite
 ##############################################################################################
 
 adn_stool_graph <- adn_all_stool %>% 
-  filter(study == "composite") %>% 
+  filter(study == "composite", 
+         measure != "all_four", 
+         measure != "total_four") %>% 
   mutate(measure = factor(measure, 
-                        levels = c("all_four", "total_four", "Fusobacterium", 
-                                   "Porphyromonas", "Peptostreptococcus", "Parvimonas"), 
-                        labels = c("Increased CRC Associated\nGenera Positivity", 
-                                   "CRC Associated\nGenera Expansion", "Fusobacterium", 
+                        levels = c("four_counts", "three_counts", "two_counts", "one_counts", 
+                                   "Fusobacterium", "Porphyromonas", "Peptostreptococcus", "Parvimonas"), 
+                        labels = c("All 4", "At Least 3", "At Least 2", "At Least 1", "Fusobacterium", 
                                    "Porphyromonas", "Peptostreptococcus", "Parvimonas"))) %>% 
   ggplot(aes(log2(est), measure, xmax=log2(upper), xmin=log2(lower), colour=measure)) + 
   coord_cartesian(xlim=c(-4.2, 4.2)) + 
@@ -49,21 +64,25 @@ adn_stool_graph <- adn_all_stool %>%
   geom_errorbarh(alpha=0.5, size = 1, height=0, show.legend = F) + 
   geom_point(size = 3, show.legend = F) + 
   labs(x = expression(Log["2"]~Relative~Risk), y = "") + theme_bw() + ggtitle("A") + 
-  scale_color_manual(values = c('#000000', '#000000', '#8B5A00', '#FFA500', '#FFC125', '#E3CF57')) + 
-  annotate("text", label = paste("Adenoma (Stool)"), x = -2.8, y = 6.4, size = 2.5) + 
-  theme(plot.title = element_text(face="bold", hjust = -0.6, size = 20), 
+  scale_color_manual(values = c('#000000', '#000000', '#000000', '#000000', 
+                                '#8B5A00', '#FFA500', '#FFC125', '#E3CF57')) + 
+  scale_y_discrete(labels=expression(All~4, At~Least~3, At~Least~2, At~Least~1, italic(Fusobacterium), 
+                                     italic(Porphyromonas), italic(Peptostreptococcus), italic(Parvimonas))) +
+  annotate("text", label = paste("Adenoma (Stool)"), x = -2.9, y = 8.4, size = 2.5) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.8, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         axis.text.y = element_text(size = 10))
 
 
 crc_stool_graph <- crc_all_stool %>% 
-  filter(study == "composite") %>% 
+  filter(study == "composite", 
+         measure != "all_four", 
+         measure != "total_four") %>% 
   mutate(measure = factor(measure, 
-                          levels = c("all_four", "total_four", "Fusobacterium", 
-                                     "Porphyromonas", "Peptostreptococcus", "Parvimonas"), 
-                          labels = c("Increased CRC Associated\nGenera Positivity", 
-                                     "CRC Associated\nGenera Expansion", "Fusobacterium", 
+                          levels = c("four_counts", "three_counts", "two_counts", "one_counts", 
+                                     "Fusobacterium", "Porphyromonas", "Peptostreptococcus", "Parvimonas"), 
+                          labels = c("All 4", "At Least 3", "At Least 2", "At Least 1", "Fusobacterium", 
                                      "Porphyromonas", "Peptostreptococcus", "Parvimonas"))) %>% 
   ggplot(aes(log2(est), measure, xmax=log2(upper), xmin=log2(lower), colour=measure)) + 
   coord_cartesian(xlim=c(-4.2, 4.2)) + 
@@ -71,21 +90,25 @@ crc_stool_graph <- crc_all_stool %>%
   geom_errorbarh(alpha=0.5, size = 1, height=0, show.legend = F) + 
   geom_point(size = 3, show.legend = F) + 
   labs(x = expression(Log["2"]~Relative~Risk), y = "") + theme_bw() + ggtitle("B") + 
-  scale_color_manual(values = c('#000000', '#000000', '#B0171F', '#FF4040', '#CD5C5C', '#FFC1C1')) + 
-  annotate("text", label = paste("Carcinoma (Stool)"), x = -2.6, y = 6.4, size = 2.5) + 
-  theme(plot.title = element_text(face="bold", hjust = -0.6, size = 20), 
+  scale_color_manual(values = c('#000000', '#000000', '#000000', '#000000', 
+                                '#B0171F', '#FF4040', '#CD5C5C', '#FFC1C1')) + 
+  scale_y_discrete(labels=expression(All~4, At~Least~3, At~Least~2, At~Least~1, italic(Fusobacterium), 
+                                     italic(Porphyromonas), italic(Peptostreptococcus), italic(Parvimonas))) +
+  annotate("text", label = paste("Carcinoma (Stool)"), x = -2.8, y = 8.4, size = 2.5) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.8, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         axis.text.y = element_text(size = 10))
 
 
 adn_tissue_graph <- adn_all_tissue %>% 
-  filter(study == "composite") %>% 
+  filter(study == "composite", 
+         measure != "all_four", 
+         measure != "total_four") %>% 
   mutate(measure = factor(measure, 
-                          levels = c("all_four", "total_four", "Fusobacterium", 
-                                     "Porphyromonas", "Peptostreptococcus", "Parvimonas"), 
-                          labels = c("Increased CRC Associated\nGenera Positivity", 
-                                     "CRC Associated\nGenera Expansion", "Fusobacterium", 
+                          levels = c("four_counts", "three_counts", "two_counts", "one_counts", 
+                                     "Fusobacterium", "Porphyromonas", "Peptostreptococcus", "Parvimonas"), 
+                          labels = c("All 4", "At Least 3", "At Least 2", "At Least 1", "Fusobacterium", 
                                      "Porphyromonas", "Peptostreptococcus", "Parvimonas"))) %>% 
   ggplot(aes(log2(est), measure, xmax=log2(upper), xmin=log2(lower), colour=measure)) + 
   coord_cartesian(xlim=c(-4.2, 4.2)) + 
@@ -93,21 +116,25 @@ adn_tissue_graph <- adn_all_tissue %>%
   geom_errorbarh(alpha=0.5, size = 1, height=0, show.legend = F) + 
   geom_point(size = 3, show.legend = F) + 
   labs(x = expression(Log["2"]~Relative~Risk), y = "") + theme_bw() + ggtitle("C") + 
-  scale_color_manual(values = c('#000000', '#000000', '#8B5A00', '#FFA500', '#FFC125', '#E3CF57')) + 
-  annotate("text", label = paste("Adenoma (Tissue)"), x = -2.6, y = 6.4, size = 2.5) + 
-  theme(plot.title = element_text(face="bold", hjust = -0.6, size = 20), 
+  scale_color_manual(values = c('#000000', '#000000', '#000000', '#000000', 
+                                '#8B5A00', '#FFA500', '#FFC125', '#E3CF57')) + 
+  scale_y_discrete(labels=expression(All~4, At~Least~3, At~Least~2, At~Least~1, italic(Fusobacterium), 
+                                     italic(Porphyromonas), italic(Peptostreptococcus), italic(Parvimonas))) + 
+  annotate("text", label = paste("Adenoma (Tissue)"), x = -2.8, y = 8.4, size = 2.5) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.8, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         axis.text.y = element_text(size = 10))
 
 
 crc_tissue_graph <- crc_all_tissue %>% 
-  filter(study == "composite") %>% 
+  filter(study == "composite", 
+         measure != "all_four", 
+         measure != "total_four") %>% 
   mutate(measure = factor(measure, 
-                          levels = c("all_four", "total_four", "Fusobacterium", 
-                                     "Porphyromonas", "Peptostreptococcus", "Parvimonas"), 
-                          labels = c("Increased CRC Associated\nGenera Positivity", 
-                                     "CRC Associated\nGenera Expansion", "Fusobacterium", 
+                          levels = c("four_counts", "three_counts", "two_counts", "one_counts", 
+                                     "Fusobacterium", "Porphyromonas", "Peptostreptococcus", "Parvimonas"), 
+                          labels = c("All 4", "At Least 3", "At Least 2", "At Least 1", "Fusobacterium", 
                                      "Porphyromonas", "Peptostreptococcus", "Parvimonas"))) %>% 
   ggplot(aes(log2(est), measure, xmax=log2(upper), xmin=log2(lower), colour=measure)) + 
   coord_cartesian(xlim=c(-4.2, 4.2)) + 
@@ -115,9 +142,12 @@ crc_tissue_graph <- crc_all_tissue %>%
   geom_errorbarh(alpha=0.5, size = 1, height=0, show.legend = F) + 
   geom_point(size = 3, show.legend = F) + 
   labs(x = expression(Log["2"]~Relative~Risk), y = "") + theme_bw() + ggtitle("D") + 
-  scale_color_manual(values = c('#000000', '#000000', '#B0171F', '#FF4040', '#CD5C5C', '#FFC1C1')) + 
-  annotate("text", label = paste("Carcinoma (Tissue)"), x = -2.5, y = 6.4, size = 2.5) + 
-  theme(plot.title = element_text(face="bold", hjust = -0.6, size = 20), 
+  scale_color_manual(values = c('#000000', '#000000', '#000000', '#000000', 
+                                '#B0171F', '#FF4040', '#CD5C5C', '#FFC1C1')) + 
+  scale_y_discrete(labels=expression(All~4, At~Least~3, At~Least~2, At~Least~1, italic(Fusobacterium), 
+                                     italic(Porphyromonas), italic(Peptostreptococcus), italic(Parvimonas))) + 
+  annotate("text", label = paste("Carcinoma (Tissue)"), x = -2.7, y = 8.4, size = 2.5) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.8, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         axis.text.y = element_text(size = 10))
@@ -130,6 +160,6 @@ crc_tissue_graph <- crc_all_tissue %>%
 select_genus_combined <- grid.arrange(adn_stool_graph, crc_stool_graph, 
                                       adn_tissue_graph, crc_tissue_graph)
 
-ggsave("results/figures/select_genus_RR_composite.pdf", 
+ggsave("results/figures/Figure3.pdf", 
        select_genus_combined, width = 8, height = 6, dpi = 300)
 
