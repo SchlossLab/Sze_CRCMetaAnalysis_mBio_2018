ce284d4527da6e275bb7606c2301bae73b62b8ea
marcsze <marcasze@gmail.com>
2017-11-06 08:45:24 -0500

    fig 2 code

---
+++ code/make_stool_alpha_RR_graph.R
@@ -0,0 +1,106 @@
+### Code to create alpha RR graph Figure 2
+### Stool specific measures only
+### Marc Sze
+
+# Load in needed generic functions
+source("code/functions.R")
+
+# Load in needed libraries
+loadLibs(c("tidyverse", "gridExtra", "viridis"))
+
+
+# Load in needed data tables (carcinoma)
+adn_all_stool <- read_csv("data/process/tables/alpha_adn_RR_composite.csv") %>% 
+  rename(est = rr, lower = ci_lb, upper = ci_ub) %>% 
+  mutate(study = "composite") %>% 
+  bind_rows(read_csv("data/process/tables/alpha_adn_RR_ind_results.csv")) %>% 
+  mutate(region = c(rep("combined", 3), rep("V1-V3", 3), rep("V4", 6), rep("V3-V5", 3)))
+
+
+crc_all_stool <- read_csv("data/process/tables/alpha_RR_composite.csv") %>% 
+  rename(est = rr, lower = ci_lb, upper = ci_ub) %>% 
+  mutate(study = "composite") %>% 
+  bind_rows(read_csv("data/process/tables/alpha_RR_ind_results.csv")) %>% 
+  mutate(region = c(rep("combined", 3), rep("V3", 3), rep("V4", 3), rep("V3-V4", 3), 
+                    rep("V4", 6), rep("V3-V5", 3), rep("V3-V4", 3)))
+
+
+### Used the `viridis_pal()(7)` to choose colors to use
+#### zeller = V4 - #440154FF
+#### hale = V3-V5 - #443A83FF
+#### brim = V1-V3 - #31688EFF
+#### baxter = V4 - #440154FF
+#### weir = V4 - #440154FF
+#### wang = V3 - #21908CFF
+### flemer = V3-V4 - #35B779FF
+### ahn = V3-V4 - #35B779FF
+### lu = V3-V4 - #35B779FF
+### dejea = V3-V5 - #443A83FF
+### geng = V1-V2 - #8FD744FF
+### sana = V1-V2 - #8FD744FF
+### burns = V5-V6 - #FDE725FF
+### chen = V1-V3 - #31688EFF
+
+##############################################################################################
+############################## List of code to make figures ##################################
+##############################################################################################
+
+adn_stool_graph <- adn_all_stool %>% 
+  mutate(study = factor(study, 
+                        levels = c("composite", "zeller", "hale", "brim", "baxter"), 
+                        labels = c( "Pooled", "Zeller", "Hale", "Brim", "Baxter")), 
+         region = factor(region, 
+                         levels = c("combined", "V4", "V3-V5", "V1-V3")), 
+         measure = factor(measure, 
+                          levels = c("sobs", "shannoneven", "shannon"), 
+                          labels = c("Observed OTUs", "Evenness", "Shannon Diversity"))) %>% 
+  ggplot(aes(log2(est), study, xmax=log2(upper), xmin=log2(lower), colour=region)) + 
+  coord_cartesian(xlim=c(-4.2, 4.2)) + 
+  geom_vline(xintercept = 0.0, linetype=2, alpha=0.75) + 
+  geom_errorbarh(alpha=0.5, size = 1, height=0, show.legend = F) + 
+  geom_point(size = 3, show.legend = F) + 
+  facet_grid(. ~ measure) + 
+  labs(x = expression(Log["2"]~Relative~Risk), y = "") + theme_bw() + ggtitle("A") + 
+  scale_color_manual(values = c( '#000000', '#440154FF', '#443A83FF', '#31688EFF')) + 
+  annotate("text", label = paste("Adenoma (Stool)"), x = -2.75, y = 5.5, size = 2.5) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.text.y = element_text(size = 10))
+
+
+
+crc_stool_graph <- crc_all_stool %>% 
+  mutate(study = factor(study, 
+                        levels = c("composite", "zeller", "weir", "wang", "hale", 
+                                   "flemer", "baxter", "ahn"), 
+                        labels = c( "Pooled", "Zeller", "Weir", "Wang",  "Hale", 
+                                    "Flemer", "Baxter", "Ahn")), 
+         region = factor(region, 
+                         levels = c("combined", "V3", "V4", "V3-V4", "V3-V5")), 
+         measure = factor(measure, 
+                          levels = c("sobs", "shannoneven", "shannon"), 
+                          labels = c("Observed OTUs", "Evenness", "Shannon Diversity"))) %>%  
+  ggplot(aes(log2(est), study, xmax=log2(upper), xmin=log2(lower), colour=region)) + 
+  coord_cartesian(xlim=c(-4.2, 4.2)) + 
+  geom_vline(xintercept = 0.0, linetype=2, alpha=0.75) + 
+  geom_errorbarh(alpha=0.5, size = 1, height=0, show.legend = F) + 
+  geom_point(size = 3, show.legend = F) + 
+  facet_grid(. ~ measure) + 
+  labs(x = expression(Log["2"]~Relative~Risk), y = "") + theme_bw() + ggtitle("B") + 
+  scale_color_manual(values = c('#000000', '#21908CFF', '#440154FF', '#35B779FF', '#443A83FF')) + 
+  annotate("text", label = paste("Carcinoma (Stool)"), x = -2.70, y = 8.4, size = 2.5) + 
+  theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.text.y = element_text(size = 10))
+
+
+##############################################################################################
+############### Run the actual programs to make the figure ###################################
+##############################################################################################
+
+stool_alpha_RR <- grid.arrange(adn_stool_graph, crc_stool_graph)
+
+ggsave("results/figures/Figure2.pdf", stool_alpha_RR, width = 8, height = 7, dpi = 300)
+
