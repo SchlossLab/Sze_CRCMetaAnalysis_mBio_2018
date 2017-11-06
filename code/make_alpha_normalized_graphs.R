e9c5233c534c0dc0c1c1209084a03e12c43d11e4
marcsze <marcasze@gmail.com>
2017-11-06 07:33:33 -0500

    update figure 1

--- code/make_alpha_normalized_graphs.R
+++ code/make_alpha_normalized_graphs.R
@@ -33,12 +33,13 @@ unmatched_tissue_data <- read_csv("data/process/tables/unmatched_tissue_normaliz
 stool_graph <- stool_data %>% 
   mutate(disease = factor(disease, 
                           levels = c("control", "polyp", "cancer"), 
-                          labels = c("Control", "Adenoma", "Cancer")), 
+                          labels = c("Control", "Adenoma", "Carcinoma")), 
          alpha_metric = factor(alpha_metric, 
                                levels = c("sobs", "shannoneven", "shannon"), 
                                labels = c("Observed OTUs", "Evenness", "Shannon Diversity"))) %>% 
   ggplot(aes(disease, measurement, color = disease)) + 
   geom_boxplot(show.legend = F) + facet_grid(. ~ alpha_metric) + 
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") + 
   scale_color_manual(values = c('#228B22', '#FFD700', '#DC143C')) + 
   labs(x = "", y = "Z-Score") + theme_bw() + ggtitle("A") + 
   annotate("text", label = paste("Stool"), x = 2, y = 3.5, size = 2.5) + 
@@ -52,12 +53,13 @@ stool_graph <- stool_data %>%
 unmatched_tissue_graph <- unmatched_tissue_data %>% 
   mutate(disease = factor(disease, 
                           levels = c("control", "polyp", "cancer"), 
-                          labels = c("Control", "Adenoma", "Cancer")), 
+                          labels = c("Control", "Adenoma", "Carcinoma")), 
          alpha_metric = factor(alpha_metric, 
                                levels = c("sobs", "shannoneven", "shannon"), 
                                labels = c("Observed OTUs", "Evenness", "Shannon Diversity"))) %>% 
   ggplot(aes(disease, measurement, color = disease)) + 
   geom_boxplot(show.legend = F) + facet_grid(. ~ alpha_metric) + 
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black") + 
   scale_color_manual(values = c('#228B22', '#FFD700', '#DC143C')) + 
   labs(x = "", y = "Z-Score") + theme_bw() + ggtitle("B") + 
   annotate("text", label = paste("Unmatched Tissue"), x = 2, y = 3, size = 2.5) + 
@@ -69,16 +71,17 @@ unmatched_tissue_graph <- unmatched_tissue_data %>%
 
 matched_tissue_graph <- matched_tissue_data %>% 
   mutate(disease = factor(disease, 
-                          levels = c("control", "cancer"), 
-                          labels = c("Control", "Cancer")), 
+                          levels = c("control", "polyp", "cancer"), 
+                          labels = c("Control", "Adenoma", "Carcinoma")), 
          alpha_metric = factor(alpha_metric, 
                                levels = c("sobs", "shannoneven", "shannon"), 
                                labels = c("Observed OTUs", "Evenness", "Shannon Diversity"))) %>% 
   ggplot(aes(disease, measurement, color = disease)) + 
   geom_boxplot(show.legend = F) + facet_grid(. ~ alpha_metric) + 
-  scale_color_manual(values = c('#228B22', '#DC143C')) + 
+  geom_hline(yintercept = 0, linetype = "dashed", color = "black")+ 
+  scale_color_manual(values = c('#228B22', '#FFD700', '#DC143C')) + 
   labs(x = "", y = "Z-Score") + theme_bw() + ggtitle("C") + 
-  annotate("text", label = paste("Matched Tissue"), x = 1.5, y = 2.3, size = 2.5) + 
+  annotate("text", label = paste("Matched Tissue"), x = 2, y = 3, size = 2.5) + 
   theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
@@ -94,7 +97,7 @@ alpha_all_graph <- grid.arrange(stool_graph, unmatched_tissue_graph, matched_tis
 
 
 ggsave("results/figures/Figure1.pdf", 
-       alpha_all_graph, width = 6, height = 9, dpi = 300)
+       alpha_all_graph, width = 6.5, height = 9, dpi = 300)
 
 
 
