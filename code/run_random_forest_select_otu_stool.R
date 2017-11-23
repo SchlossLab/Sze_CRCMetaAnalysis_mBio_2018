36ccd0bef18c2dbbfad90b66fa07281428a8f2f4
marcsze <marcasze@gmail.com>
2017-11-23 18:09:32 -0500

    add a Hale study pvalue rule

--- code/run_random_forest_select_otu_stool.R
+++ code/run_random_forest_select_otu_stool.R
@@ -293,7 +293,15 @@ make_summary_data <- function(i, model_info, dataList, a_summary, r_summary,
   worst_random_roc <- roc(dataList[[random_name]]$disease ~ 
                             model_info[["random_mod"]][["min_model"]][["pred"]][, "cancer"])
   # Generate a p-value on whether the distribution between actual and random are different
-  pvalue <- t.test(a_summary$ROC, r_summary$ROC)$p.value
+  if(i != "hale"){
+    
+    pvalue <- t.test(a_summary$ROC, r_summary$ROC)$p.value
+  } else{
+    
+    pvalue <- NA
+  }
+  
+  
   # Create a final list with all the needed ROC curve data and respective p-value
   finalData <- list(
     all_data = cbind(
@@ -380,7 +388,7 @@ for(i in c(stool_sets, "flemer")){
 
 
 # Write out the relevant data frames
-write.csv(all_roc_data, "data/process/tables/stool_rf_select_tu_roc.csv", row.names = F)
+write.csv(all_roc_data, "data/process/tables/stool_rf_select_otu_roc.csv", row.names = F)
 write.csv(all_comparisons, "data/process/tables/stool_rf_select_otu_random_comparison_summary.csv", 
           row.names = F)
 
