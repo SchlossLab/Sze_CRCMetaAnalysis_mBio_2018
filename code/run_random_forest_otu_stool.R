1a44d2d2f146f9cdfd00d4c47b7082f2250a6f18
marcsze <marcasze@gmail.com>
2017-11-25 17:58:55 -0500

    fix overwrite error

--- code/run_random_forest_otu_stool.R
+++ code/run_random_forest_otu_stool.R
@@ -379,8 +379,8 @@ for(i in c(stool_sets, "flemer")){
 
 
 # Write out the relevant data frames
-write.csv(all_roc_data, "data/process/tables/stool_rf_select_tu_roc.csv", row.names = F)
-write.csv(all_comparisons, "data/process/tables/stool_rf_select_otu_random_comparison_summary.csv", 
+write.csv(all_roc_data, "data/process/tables/stool_rf_select_otu_roc.csv", row.names = F)
+write.csv(all_comparisons, "data/process/tables/stool_rf_otu_random_comparison_summary.csv", 
           row.names = F)
 
 
