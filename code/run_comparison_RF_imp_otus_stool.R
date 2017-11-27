3c302852bb938f3f1c768bfe9edfc304c06c98f1
marcsze <marcasze@gmail.com>
2017-11-27 11:37:36 -0500

    first commit

---
+++ code/run_comparison_RF_imp_otus_stool.R
@@ -0,0 +1,27 @@
+### Run Random Forest Analysis OTU Level - Tissue
+### Generate model and then test on remaining studies
+### Marc Sze
+
+## For tissue set CV to 5 since there are in general less samples per study
+
+# Load in needed functions and libraries
+source('code/functions.R')
+
+# Load needed libraries
+loadLibs(c("tidyverse", "caret", "pROC"))
+
+
+##############################################################################################
+########################## Group of Functions needed to run the analysis #####################
+##############################################################################################
+
+
+
+
+
+
+
+
+##############################################################################################
+########################## Code used to run the analysis  ####################################
+##############################################################################################
