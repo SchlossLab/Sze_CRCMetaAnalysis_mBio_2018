1f38b30677d122f13f3a67eb52a0851a752bd9a5
marcsze <marcasze@gmail.com>
2018-02-07 14:09:30 -0500

    change to OR

--- code/get_adenoma_specific_genus_analysis_tissue.R
+++ code/get_adenoma_specific_genus_analysis_tissue.R
@@ -190,7 +190,7 @@ run_rr <- function(high_low_vector, disease_vector){
   # runs the RR test based on the obtained 2x2 table
   test <- try(epi.2by2(contingency, method="cohort.count"))
   # Pull only specific information from the stored list in "test"
-  test_values <- try(cbind(test$massoc$RR.strata.score, 
+  test_values <- try(cbind(test$massoc$OR.strata.score, 
                            pvalue = test$massoc$chisq.strata$p.value))
   # store both the obtained raw counts and the resulting RR with pvalue
   combined_data <- try(list(data_tbl = contingency, test_values = test_values))
@@ -265,7 +265,7 @@ run_pooled <- function(alpha_d, dataset){
   # Run the actual pooled test
   rr_pooled_test <- rma(ai = high_Y, bi = high_N, 
                         ci = low_Y, di = low_N, data = test_data, 
-                        measure = "RR", method = "REML")
+                        measure = "OR", method = "REML")
   # Store a vector of the important results of interest
   results <- c(exp(c(rr = rr_pooled_test$b[[1, 1]], ci_lb = rr_pooled_test$ci.lb, 
                      ci_ub=rr_pooled_test$ci.ub)), pvalue = rr_pooled_test$pval, 
@@ -387,9 +387,9 @@ pooled_results <- t(sapply(all_genera,
 write.csv(counts_data, 
           "data/process/tables/adn_select_genus_tissue_group_counts_summary.csv", 
           row.names = F)
-write.csv(RR_data, "data/process/tables/adn_select_genus_RR_tissue_ind_results.csv", 
+write.csv(RR_data, "data/process/tables/adn_select_genus_OR_tissue_ind_results.csv", 
           row.names = F)
-write.csv(pooled_results, "data/process/tables/adn_select_genus_RR_tissue_composite.csv", 
+write.csv(pooled_results, "data/process/tables/adn_select_genus_OR_tissue_composite.csv", 
           row.names = F)
 
 
