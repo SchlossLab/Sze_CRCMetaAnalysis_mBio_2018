b266fd3848d17ed861fc9e5864e856d314a15742
marcsze <marcasze@gmail.com>
2018-02-22 11:03:20 -0500

    remove adn and swithc to total studies

--- code/make_stool_imp_otu_graph.R
+++ code/make_stool_imp_otu_graph.R
@@ -10,18 +10,11 @@ loadLibs(c("tidyverse", "gridExtra", "viridis", "stringr"))
 # Read in Needed data
 crc_genera_occurances <- read_csv("data/process/tables/crc_RF_genera_stool_top10.csv") %>% 
   mutate(otu = str_replace_all(otu, "_unclassified", ""), 
-         otu = str_replace_all(otu, "\\.", "/"))
-adn_genera_occurances <- read_csv("data/process/tables/adn_RF_genera_stool_top10.csv") %>% 
-  mutate(otu = str_replace_all(otu, "_unclassified", ""), 
-         otu = str_replace_all(otu, "\\.", "/"))
+         otu = str_replace_all(otu, "\\.Shigella", ""))
 crc_otu_occurances <- read_csv("data/process/tables/crc_RF_otu_stool_top10.csv") %>% 
   mutate(genus = str_replace_all(genus, "_unclassified", ""), 
-         genus = str_replace_all(genus, "\\.", "/"))
-adn_otu_occurances <- read_csv("data/process/tables/adn_RF_otu_stool_top10.csv") %>% 
-  mutate(genus = str_replace_all(genus, "_unclassified", ""), 
-         genus = str_replace_all(genus, "\\.", "/"))
+         genus = str_replace_all(genus, "\\/Shigella", ""))
 
-adn_stool_sets <- 4
 crc_stool_sets <- 6
 
 
@@ -29,58 +22,20 @@ crc_stool_sets <- 6
 ############################## List of code to make figures ##################################
 ##############################################################################################
 
-adn_genera <- adn_genera_occurances %>% arrange(occurance) %>% 
-  mutate(otu = str_replace(otu, "_", " "), 
-    otu = factor(otu, 
-                      levels = otu,
-                      labels = otu), 
-    occurance = occurance/adn_stool_sets) %>% 
-  ggplot(aes(otu, occurance)) + 
-  geom_bar(stat = "identity", fill = '#8B8B83') + 
-  theme_bw() + 
-  labs(x = "", y = "Occurrence Across Studies") + 
-  coord_flip(ylim = c(0, 1)) + 
-  ggtitle("A") + 
-  scale_y_continuous(labels = scales::percent, expand = c(0,0.03)) + 
-  theme(axis.text.y = element_text(face = "italic"), 
-        plot.title = element_text(face="bold", hjust = -0.3, size = 20), 
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank())
-
-
 
 crc_genera <- crc_genera_occurances %>% arrange(occurance) %>% 
   mutate(otu = str_replace(otu, "_", " "), 
          otu = factor(otu, 
                       levels = otu,
                       labels = otu), 
-         occurance = occurance/crc_stool_sets) %>% 
+         occurance = occurance) %>% 
   ggplot(aes(otu, occurance)) + 
-  geom_bar(stat = "identity", fill = "black") + 
+  geom_bar(stat = "identity", fill = '#DB7093') + 
   theme_bw() + 
   labs(x = "", y = "Occurrence Across Studies") + 
-  coord_flip(ylim = c(0, 1)) + 
-  ggtitle("B") + 
-  scale_y_continuous(labels = scales::percent, expand = c(0,0.03)) + 
-  theme(axis.text.y = element_text(face = "italic"), 
-        plot.title = element_text(face="bold", hjust = -0.3, size = 20), 
-        panel.grid.major = element_blank(), 
-        panel.grid.minor = element_blank())
-
-
-adn_otu <- adn_otu_occurances %>% arrange(occurance) %>% 
-  mutate(genus = str_replace(genus, "_", " "), 
-         genus = factor(genus, 
-                      levels = genus,
-                      labels = genus), 
-         occurance = occurance/adn_stool_sets) %>% 
-  ggplot(aes(genus, occurance)) + 
-  geom_bar(stat = "identity", fill = '#8B8B83') + 
-  theme_bw() + 
-  labs(x = "", y = "Occurrence Across Studies") + 
-  coord_flip(ylim = c(0, 1)) + 
-  ggtitle("C") + 
-  scale_y_continuous(labels = scales::percent, expand = c(0,0.03)) + 
+  coord_flip(ylim = c(0, 6)) + 
+  ggtitle("A") + 
+  scale_y_continuous(expand = c(0,0.03)) + 
   theme(axis.text.y = element_text(face = "italic"), 
         plot.title = element_text(face="bold", hjust = -0.3, size = 20), 
         panel.grid.major = element_blank(), 
@@ -92,15 +47,14 @@ crc_otu <- crc_otu_occurances %>% arrange(occurance) %>%
          genus = factor(genus, 
                         levels = genus,
                         labels = genus), 
-         occurance = occurance/crc_stool_sets) %>% 
+         occurance = occurance) %>% 
   ggplot(aes(genus, occurance)) + 
-  geom_bar(stat = "identity", fill = 'black') + 
+  geom_bar(stat = "identity", fill = '#B0171F') + 
   theme_bw() + 
   labs(x = "", y = "Occurrence Across Studies") + 
-  coord_flip(ylim = c(0, 1)) + 
-  ggtitle("D") + 
-  scale_y_continuous(labels = scales::percent, 
-                     expand = c(0,0.03)) + 
+  coord_flip(ylim = c(0, 6)) + 
+  ggtitle("B") + 
+  scale_y_continuous(expand = c(0,0.03)) + 
   theme(axis.text.y = element_text(face = "italic"), 
         plot.title = element_text(face="bold", hjust = -0.3, size = 20), 
         panel.grid.major = element_blank(), 
@@ -112,11 +66,11 @@ crc_otu <- crc_otu_occurances %>% arrange(occurance) %>%
 ##############################################################################################
 
 
-stool_graph <- grid.arrange(adn_genera, crc_genera, adn_otu, crc_otu)
+stool_graph <- grid.arrange(crc_genera, crc_otu, nrow = 1, ncol = 2)
 
 
 ggsave("results/figures/Figure4.pdf", 
-       stool_graph, width = 11, height = 8, dpi = 300)
+       stool_graph, width = 8, height = 6, dpi = 300)
 
 
 
