833e0825bde5cbcf87c05e062c2691ff0d20d89f
marcsze <marcasze@gmail.com>
2018-02-21 22:41:26 -0500

    add code for counts

--- code/Run_sig_taxa_AUC_generation.R
+++ code/Run_sig_taxa_AUC_generation.R
@@ -6,7 +6,7 @@
 source('code/functions.R')
 
 # Load needed libraries
-loadLibs(c("tidyverse", "caret", "pROC"))
+loadLibs(c("tidyverse", "caret", "pROC", "AUC"))
 
 # Stool Only sets
 # Hale, Wang, Brim, Weir, Ahn, Zeller, Baxter
@@ -90,20 +90,86 @@ assign_disease <- function(studies, generaData, metaData, dataList){
 
 
 # Function to generate step-wise increase in thresholds
-get_thresholds <- function(study, col_of_int, dataList){
+get_thresholds <- function(study, col_of_int, total_nums, dataList){
   
   tempData <- dataList[[study]] %>% 
     select(one_of(col_of_int))
   
   tempMax <- apply(tempData, 2, function(x) max(x))
   
+  tempInterval <- tempMax/total_nums
   
-  return(tempMax)
+  actualIntervals <- list()
+  
+  for(i in 1:length(col_of_int)){
+    
+    actualIntervals[[col_of_int[i]]] <- sapply(c(1:total_nums), 
+                              function(x) tempInterval[i] * x)
+  }
+  
+  actualIntervals <- actualIntervals %>% bind_rows()
+  
+  
+  return(actualIntervals)
+  
+}
+
+# Function to generate counts (TP, TN, FP, FN) from thresholds
+generate_counts <- function(study, col_of_int, thresholdList, dataList){
+  
+  tempData <- as.data.frame(dataList[[study]])
+  tempThreshold <- as.data.frame(thresholdList[[study]])
+  tempDisease <- tempData$disease
+  
+  
+  tempGeneraAnalysis <- sapply(col_of_int, 
+                               function(x) making_counts(x, tempThreshold, tempData), simplify = F)
+  
+  
+  test_counts <- sapply(col_of_int, 
+                        function(x) get_tp_tn(x, tempDisease, tempGeneraAnalysis), simplify = F)
+  
+  return(test_counts)
   
 }
 
 
-test <- get_thresholds("baxter", combined_genera, select_good_data)
+# Function to generate the disease calls from thresholds
+making_counts <- function(taxa, thresholds, dataTable){
+  
+  tempVector <- dataTable[, taxa]
+  tempThreshold <- thresholds[, taxa]
+  
+  tempTest <- sapply(1:length(tempThreshold), 
+                     function(x) ifelse(tempVector < tempThreshold[x], 
+                                        invisible("control"), invisible("cancer")), simplify = F)
+  
+  
+  return(tempTest)
+  
+}
+
+# Function to generate the actual counts
+get_tp_tn <- function(taxa, diseaseVector, dataList){
+  
+  tempList <- dataList[[taxa]]
+  tempDataStore <- list()
+  
+  tempAUC <- sapply(1:length(tempList), 
+                    function(x) auc(roc(tempList[[x]], diseaseVector)), simplify = F)
+  
+#  for(interval in tempList){
+
+    
+#    tempAUC <- auc(roc(interval, diseaseVector))
+    
+#  }
+  
+  return(tempAUC)
+  
+}
+
+
 
 ##############################################################################################
 ############### Run the actual programs to get the data (CRC Specific Genera) ################
@@ -126,9 +192,19 @@ rr_data <- read_csv("data/process/tables/select_genus_OR_stool_composite.csv") %
 combined_genera <- rr_data$measure
 
 # pick out only significant taxa from each data set
-
 select_good_data <- sapply(c(stool_sets, "flemer"), 
                            function(x) select(good_datasets[[x]], 
                                               disease, one_of(combined_genera)), simplify = F)
+
+# Get the thresholds for each study
+study_thresholds <- sapply(c(stool_sets, "flemer"), 
+                           function(x) get_thresholds(x, combined_genera, 10, select_good_data), simplify = F)
+
+
+# Generate the counts for each study and each taxa
+count_data_list <- sapply(c(stool_sets, "flemer"), 
+                          function(x) generate_counts(x, combined_genera, study_thresholds, select_good_data), simplify = F)
+
+
 
 
