3d367235fd4f8f2ddf3231ef5c3fcb2d7f6fb82f
marcsze <marcasze@gmail.com>
2018-02-28 13:14:17 -0500

    update fig 3 with pat's comments

--- code/make_ind_genera_auc_graph.R
+++ code/make_ind_genera_auc_graph.R
@@ -17,7 +17,9 @@ stool_data <- read_csv("data/process/tables/ind_genera_auc_stool.csv") %>%
                        levels = c("Clostridium_XI", "Ruminococcus", "Enterobacteriaceae", "Escherichia", "Fusobacterium", 
                                   "Parvimonas", "Peptostreptococcus", "Porphyromonas"), 
                        labels = c("Clostridium XI", "Ruminococcus", "Enterobacteriaceae", "Escherichia", "Fusobacterium", 
-                                  "Parvimonas", "Peptostreptococcus", "Porphyromonas")))
+                                  "Parvimonas", "Peptostreptococcus", "Porphyromonas"))) %>% 
+  
+  mutate(auc = ifelse(taxa == "Ruminococcus" | taxa == "Clostridium XI", invisible(1-auc), invisible(auc)))
 
 combined_stool <- stool_data %>% group_by(taxa) %>% 
   summarise(auc = median(auc)) %>% 
@@ -28,13 +30,15 @@ combined_stool <- stool_data %>% group_by(taxa) %>%
                                   "Parvimonas", "Peptostreptococcus", "Porphyromonas"), 
                        labels = c("Clostridium XI", "Ruminococcus", "Enterobacteriaceae", "Escherichia", "Fusobacterium", 
                                   "Parvimonas", "Peptostreptococcus", "Porphyromonas")))
+
   
 
 unmatched_tissue_data <- read_csv("data/process/tables/ind_genera_auc_unmatched_tissue.csv") %>% 
   mutate(taxa = str_replace_all(taxa, "_unclassified", ""), 
          taxa = factor(taxa, 
                        levels = c("Dorea", "Weissella", "Blautia"), 
-                       labels = c("Dorea", "Weissella", "Blautia")))
+                       labels = c("Dorea", "Weissella", "Blautia"))) %>% 
+  mutate(auc = ifelse(taxa == "Dorea" | taxa == "Blautia", invisible(1-auc), invisible(auc)))
 
 combined_unmatched_tissue <- unmatched_tissue_data %>% group_by(taxa) %>% 
   summarise(auc = median(auc)) %>% 
@@ -50,19 +54,22 @@ combined_unmatched_tissue <- unmatched_tissue_data %>% group_by(taxa) %>%
 
 
 stool_graph <- stool_data %>% 
-  ggplot(aes(auc, taxa, color = taxa)) + 
+  mutate(study = factor(study, 
+                 levels = c("ahn", "baxter", "flemer", "hale", "wang", "weir", "zeller"), 
+                 labels = c("Ahn", "Baxter", "Flemer", "Hale", "Wang", "Weir", "Zeller"))) %>% 
+  ggplot(aes(auc, taxa, color = study)) + 
   geom_vline(xintercept = 0.5, linetype = "dashed") + 
-  geom_point(show.legend = F) + 
-  geom_point(data = combined_stool, aes(auc, taxa, color = taxa), size = 3.5, show.legend = F) + 
+  geom_point(show.legend = T) + 
+  geom_point(data = combined_stool, aes(auc, taxa), color = "black", size = 3.5, show.legend = F) + 
   coord_cartesian(xlim = c(0, 1)) + 
   theme_bw() + 
-  scale_color_manual(values = c('#C1C1C1', '#B7B7B7', '#AAAAAA', '#8E8E8E', '#848484', 
-                                '#5B5B5B', '#515151', '#282828')) + 
+  scale_color_manual(name = "", values = c('#B0C4DE', '#8968CD', '#ED9121', '#006400', '#97D83FFF', '#8B4513', '#FDE725FF')) + 
   labs(x = "AUC", y = "") + ggtitle("A") + 
   annotate("text", label = paste("Carcinoma (Stool)"), x = 0.15, y = 8.4, size = 2.5) + 
   theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
+        legend.position = "bottom", 
         axis.text.y = element_text(face = "italic", size = 10), 
         axis.text.x = element_text(size = 10))
   
@@ -70,20 +77,25 @@ stool_graph <- stool_data %>%
 
 
 tissue_graph <- unmatched_tissue_data %>% 
-  ggplot(aes(auc, taxa, color = taxa)) + 
+  mutate(study = factor(study, 
+                        levels = c("burns", "chen",  "flemer", "sana"), 
+                        labels = c("Burns", "Chen", "Flemer", "Sanapareddy"))) %>% 
+  ggplot(aes(auc, taxa, color = study)) + 
   geom_vline(xintercept = 0.5, linetype = "dashed") + 
-  geom_point(show.legend = F) + 
-  geom_point(data = combined_unmatched_tissue, aes(auc, taxa, color = taxa), size = 3.5, show.legend = F) + 
-  coord_cartesian(xlim = c(0, 1)) + 
+  geom_point(show.legend = T) + 
+  geom_point(data = combined_unmatched_tissue, aes(auc, taxa), color = "black", size = 3.5, show.legend = F) + 
   theme_bw() + 
-  scale_color_manual(values = c('#C1C1C1', '#8E8E8E', '#282828')) + 
+  scale_color_manual(name = "", values = c('#453581FF', '#CD6889', '#ED9121', '#8EE5EE'), 
+                     guide = guide_legend(nrow = 2, ncol = 2)) + 
   labs(x = "AUC", y = "") + ggtitle("B") + 
-  annotate("text", label = paste("Carcinoma (Unmatched Tissue)"), x = 0.2, y = 3.5, size = 2.5) + 
+  annotate("text", label = paste("Carcinoma\n(Unmatched Tissue)"), x = 0.2, y = 3.4, size = 2.5) + 
   theme(plot.title = element_text(face="bold", hjust = -0.07, size = 20), 
         panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
+        legend.position = "bottom", 
         axis.text.y = element_text(face = "italic", size = 10), 
-        axis.text.x = element_text(size = 10))
+        axis.text.x = element_text(size = 10)) + 
+  coord_fixed(xlim = c(0, 1), ratio = 0.3)
 
 
 ##############################################################################################
